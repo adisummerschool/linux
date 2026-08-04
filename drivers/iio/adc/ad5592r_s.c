09bdb34f2643249c44386be7d6641d07ef31b6b6
Flavius Moldovan <flaviuss3035@gmail.com>
2026-08-04 17:15:36 +0300

    drivers: iio: adc: Added initial driver and configuration for ad5592r_s
    Added ad5592r_s.c with main SPI driver logic, IIO device registration,
    and probe/remove operations.
    Integrated Makefile into the Linux kernel build system.
    Added CONFIG_AD5592R_S option to Kconfig with SPI and IIO dependencies.
    Added AD5592R_S symbol to Kconfig.adi under the global IIO_ALL_ADI_DRIVERS
    configuration package.
    Added hardware node definition to zynq-coraz7s.dts on spi0 (CS0, 10MHz)
    matching the driver compatible string.
    
    Signed-off-by: Flavius Moldovan <flaviuss3035@gmail.com>

---
+++ drivers/iio/adc/ad5592r_s.c
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * IIO CORA SPI ADC driver
+ *
+ * Copyright 2011 Analog Devices Inc.
+ */
+
+ #include <linux/module.h>
+ #include <linux/spi/spi.h>
+ #include <linux/iio/iio.h>
+
+ static const struct iio_info iio_adc_info = {
+
+ };
+
+ static int iio_adc_probe(struct spi_device *spi){
+
+    struct iio_dev *indio_dev;
+    
+    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+
+    indio_dev->name = "ad5592r_s";
+    indio_dev->info = &iio_adc_info;
+
+    return devm_iio_device_register(&spi->dev, indio_dev);
+ }
+
+ static struct spi_driver iio_adc_driver = {
+    .driver = {
+        .name = "ad5592r_s"
+    },
+    .probe = iio_adc_probe
+ };
+ module_spi_driver(iio_adc_driver);
+ 
+ MODULE_AUTHOR("Moldovan Flavius <flaviuss3035@gmail.com>");
+ MODULE_DESCRIPTION("Analog Devices IIO CORA Summer School");
+ MODULE_LICENSE("GPL v2");
