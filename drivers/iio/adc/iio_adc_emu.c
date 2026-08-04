6d8f5f5172f446f1cbb8111ac5d24057e1a7c6f8
Doaga Petrut-Stefan <doaga.petrut.stefan@gmail.com>
2026-08-04 12:20:36 +0300

    Fixed
    Signed-off-by: Doaga Petrut-Stefan <doaga.petrut.stefan@gmail.com>

---
+++ drivers/iio/adc/iio_adc_emu.c
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * SPI ADC driver
+ *
+ * Copyright 2011 Analog Devices Inc (from AD5592 Driver)
+ * Copyright 2012 CS Systemes d'Information
+ */
+
+ #include <linux/module.h>
+ #include <linux/spi/spi.h>
+ #include <linux/iio/iio.h>
+
+ static const struct iio_info iio_adc_emu_info = {
+
+ };
+
+ static int iio_adc_emu_probe(struct spi_device *spi)
+ {
+    struct iio_dev *indio_dev;
+
+    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+
+    indio_dev->name = "iio_adc_emu";
+    indio_dev->info = &iio_adc_emu_info;
+
+    return devm_iio_device_register(&spi->dev, indio_dev);
+ }
+
+ static struct spi_driver iio_adc_emu_driver = {
+    .driver = {
+        .name = "iio_adc_emu"
+    },
+    .probe = iio_adc_emu_probe
+ };
+
+ module_spi_driver(iio_adc_emu_driver);
+
+MODULE_AUTHOR("Doaga Petrut-Stefan <doaga.petrut.stefan@gmail.com");
+MODULE_DESCRIPTION("Analog Devices IIO ADC EMU Summer School");
+MODULE_LICENSE("GPL v2");
