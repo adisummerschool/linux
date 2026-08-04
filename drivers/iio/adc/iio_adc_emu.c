a9c01953f8145f8a3ca485c6efa2dee1340f3e8e
Dradici Leon <dradici.leon5@gmail.com>
2026-08-04 12:08:44 +0300

    drivers: iio: adc: add support for emu
    Added initial driver for iio-adc-emu, with the name included.
    Added module to Kconfig, Makefile and Kconfig.adi.
    Added devicetree for versatile-pb-adi-emu.
    
    Signed-off-by: Dradici Leon <dradici.leon5@gmail.com>

---
+++ drivers/iio/adc/iio_adc_emu.c
@@ -0,0 +1,40 @@
+// SPDX-Licennse-Identifier: GPL-2.0
+/*
+ * IIO-EMU SPI ADC Driver
+ *
+ * Copyright 2013-2015 Analog Devices Inc.
+ *
+ * Licensed under the GPL-2.
+ */
+
+ #include <linux/module.h>
+ #include <linux/spi/spi.h>
+ #include <linux/iio/iio.h>
+
+ static const struct iio_info iio_adc_emu_info = {
+
+ };
+
+ static int iio_adc_emu_probe(struct spi_device *spi)
+ {
+    struct iio_dev *indio_dev;
+
+    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+
+    indio_dev->name = "iio_adc_emu";
+    indio_dev->info = &iio_adc_emu_info;
+
+    return devm_iio_device_register(&spi->dev, indio_dev);
+ }
+
+ static struct spi_driver iio_adc_emu_driver = {
+    .driver = {
+        .name = "iio_adc_emu"
+    },
+    .probe = iio_adc_emu_probe
+ };
+ module_spi_driver(iio_adc_emu_driver);
+
+ MODULE_AUTHOR("Dradici Leon <dradici.leon5@gmail.com>");
+ MODULE_DESCRIPTION("Analog Devices IIO-EMU SPI ADC Driver");
+ MODULE_LICENSE("GPL v2");
