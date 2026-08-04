3e6fce4f4a085795b450a199ed1c24d5e5f1ea0c
Codrin Berinde <codrin.berinde@gmail.com>
2026-08-04 12:08:53 +0300

    drivers/iio/adc: added support for emu
    - added initial driver for iio-adc-emu, with the name included
    - added module to Kconfig, Makefile and Kconfig.adi
    
    Signed-off-by: Codrin Berinde <codrin.berinde@gmail.com>

---
+++ drivers/iio/adc/iio_adc_emu.c
@@ -0,0 +1,31 @@
+#include <linux/module.h>
+#include <linux/spi/spi.h>
+#include <linux/iio/iio.h>
+
+static const struct iio_info iio_adc_emu_info = {
+
+};
+
+static int iio_adc_emu_probe(struct spi_device *spi) {
+    struct iio_dev *indio_dev;
+
+    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+
+    indio_dev->name = "iio_adc_emu";
+    indio_dev->info = &iio_adc_emu_info;
+
+    return devm_iio_device_register(&spi->dev, indio_dev);
+}
+
+static struct spi_driver iio_adc_emu_driver = {
+    .driver = {
+        .name = "iio_adc_emu"
+    },
+    .probe = iio_adc_emu_probe
+};
+
+module_spi_driver(iio_adc_emu_driver);
+
+MODULE_AUTHOR("Berinde Codrin <codrin.berinde@gmail.com>");
+MODULE_DESCRIPTION("IIO ADC EMU");
+MODULE_LICENSE("GPL v2");
