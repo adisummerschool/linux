53b4f08de554b657470092244e86580a683cfe10
Doaga Petrut-Stefan <doaga.petrut.stefan@gmail.com>
2026-08-05 12:43:23 +0300

    drivers/iio/adc: AD5592R read and write
    Declare ad5592r channels.
    Add read and write functions
    
    Signed-off-by: Doaga Petrut-Stefan <doaga.petrut.stefan@gmail.com>

--- drivers/iio/adc/iio_ad5592r_s.c
+++ drivers/iio/adc/iio_ad5592r_s.c
@@ -10,8 +10,96 @@
  #include <linux/spi/spi.h>
  #include <linux/iio/iio.h>
 
- static const struct iio_info iio_ad5592r_info = {
+ static int iio_ad5592r_read_raw(struct iio_dev *indio_dev,
+                                 struct iio_chan_spec const *chan,
+                                 int *val, int *val2, long mask)
+                                 {
+                                    switch (mask) {
+                                       case IIO_CHAN_INFO_RAW:
+                                             switch(chan->channel) {
+                                                case 0: *val = 0; break;
+                                                case 1: *val = 1; break;
+                                                case 2: *val = 2; break;
+                                                case 3: *val = 3; break;
+                                                case 4: *val = 4; break;
+                                                case 5: *val = 5; break;
+                                                default: return -EINVAL;
+                                             }
+                                             return IIO_VAL_INT;
+                                       default:
+                                             return -EINVAL;
+                                    }
+                                 }
 
+ static int iio_ad5592r_write_raw(struct iio_dev *indio_dev,
+                                 struct iio_chan_spec const *chan,
+                                 int val,
+                                 int val2,
+                                 long mask)
+ {
+   switch (mask) {
+      case IIO_CHAN_INFO_RAW:
+         switch(chan->channel) {
+            case 0: dev_info(&indio_dev->dev, "Trying to write to channel 0"); return 0;
+            case 1: dev_info(&indio_dev->dev, "Trying to write to channel 1"); return 0;
+            case 2: dev_info(&indio_dev->dev, "Trying to write to channel 2"); return 0;
+            case 3: dev_info(&indio_dev->dev, "Trying to write to channel 3"); return 0;
+            case 4: dev_info(&indio_dev->dev, "Trying to write to channel 4"); return 0;
+            case 5: dev_info(&indio_dev->dev, "Trying to write to channel 5"); return 0;
+            default: return -EINVAL;
+         }
+      default: 
+         return -EINVAL;
+   }
+ }
+
+ static const struct iio_chan_spec iio_ad5592r_channels[] = {
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 0,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   },
+
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 1,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   },
+
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 2,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   },
+
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 3,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   },
+
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 4,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   },
+
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 5,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   }
+ };
+
+ static const struct iio_info iio_ad5592r_info = {
+   .read_raw = &iio_ad5592r_read_raw,
+   .write_raw = &iio_ad5592r_write_raw
  };
 
  static int iio_ad5592r_probe(struct spi_device *spi)
@@ -22,6 +110,8 @@
 
     indio_dev->name = "iio_ad5592r_s";
     indio_dev->info = &iio_ad5592r_info;
+    indio_dev->channels = iio_ad5592r_channels;
+    indio_dev->num_channels = 6;
 
     return devm_iio_device_register(&spi->dev, indio_dev);
  }
