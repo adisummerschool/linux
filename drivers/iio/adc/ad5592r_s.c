f2f95cc79b4b76340552601c1301ed8cec134efc
Cosmin Strava <kosmin.strava@gmail.com>
2026-08-05 12:17:25 +0300

    drivers: iio: adc: add IIO channels for AD5592R
    Add six voltage channels for the AD5592R ADC
    Implement test read and write raw callbacks
    Register channel information with the IIO device
    Enable direct mode access
    
    Signed-off-by: Cosmin Strava <kosmin.strava@gmail.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -9,26 +9,87 @@
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
 
+static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
+			      const struct iio_chan_spec *chan,
+			      int *val,
+			      int *val2,
+			      long mask)
+{
+	switch (mask) {
+	case IIO_CHAN_INFO_RAW:
+		*val = chan->channel * 100;
+
+		return IIO_VAL_INT;
+
+	default:
+		return -EINVAL;
+	}
+}
+
+static int ad5592r_s_write_raw(struct iio_dev *indio_dev,
+			       const struct iio_chan_spec *chan,
+			       int val,
+			       int val2,
+			       long mask)
+{
+	switch (mask) {
+	case IIO_CHAN_INFO_RAW:
+		dev_info(&indio_dev->dev,
+			 "Try to write value %d to channel %d\n",
+			 val, chan->channel);
+
+		return 0;
+
+	default:
+		return -EINVAL;
+	}
+}
+
+#define AD5592R_S_CHANNEL(_channel)			\
+	{						\
+		.type = IIO_VOLTAGE,			\
+		.indexed = 1,				\
+		.channel = (_channel),			\
+		.info_mask_separate =			\
+			BIT(IIO_CHAN_INFO_RAW),		\
+	}
+
+static const struct iio_chan_spec ad5592r_s_channels[] = {
+	AD5592R_S_CHANNEL(0),
+	AD5592R_S_CHANNEL(1),
+	AD5592R_S_CHANNEL(2),
+	AD5592R_S_CHANNEL(3),
+	AD5592R_S_CHANNEL(4),
+	AD5592R_S_CHANNEL(5),
+};
+
 static const struct iio_info ad5592r_s_info = {
+	.read_raw = ad5592r_s_read_raw,
+	.write_raw = ad5592r_s_write_raw,
 };
 
 static int ad5592r_s_probe(struct spi_device *spi)
 {
-    struct iio_dev *indio_dev;
+	struct iio_dev *indio_dev;
 
-    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+	if (!indio_dev)
+		return -ENOMEM;
 
-    indio_dev->name = "ad5592r_s";
-    indio_dev->info = &ad5592r_s_info;
+	indio_dev->name = "ad5592r_s";
+	indio_dev->info = &ad5592r_s_info;
+	indio_dev->modes = INDIO_DIRECT_MODE;
+	indio_dev->channels = ad5592r_s_channels;
+	indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_channels);
 
-    return devm_iio_device_register(&spi->dev, indio_dev);
+	return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
 static struct spi_driver ad5592r_s_driver = {
-    .driver = {
-        .name = "ad5592r_s",
-    },
-    .probe = ad5592r_s_probe,
+	.driver = {
+		.name = "ad5592r_s",
+	},
+	.probe = ad5592r_s_probe,
 };
 
 module_spi_driver(ad5592r_s_driver);
