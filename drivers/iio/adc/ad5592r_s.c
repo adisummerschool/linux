77607867faad07b3c3fa93e55e165f478dc5652e
Flavius Moldovan <flaviuss3035@gmail.com>
2026-08-05 12:25:53 +0300

    drivers: iio: adc: Print written raw values in write_raw callback
    Added 6 voltage channels to iio_adc_cora_channels representing 3 dual-channel axes from the 8 available on hardware.
    Added raw read support returning dummy channel values in iio_adc_cora_read_raw.
    Added raw write logging functionality displaying input values for all 6 active channels in iio_adc_cora_write_raw.
    Added updated channel count (6) in iio_adc_probe registration.
    
    Signed-off-by: Flavius Moldovan <flaviuss3035@gmail.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -9,8 +9,102 @@
  #include <linux/spi/spi.h>
  #include <linux/iio/iio.h>
 
- static const struct iio_info iio_adc_info = {
+ static int iio_adc_cora_read_raw(struct iio_dev *indio_dev, 
+                              struct iio_chan_spec const *chan, 
+                              int *val, 
+                              int *val2, 
+                              long mask)
+{
+    switch(mask) {
+      case IIO_CHAN_INFO_RAW:
+            if (chan->channel == 0)
+               *val = 67;
+            else if (chan->channel == 1)
+               *val = 76;
+            else if (chan->channel == 2)
+               *val = 85;
+            else if (chan->channel == 3)
+               *val = 94;
+            else if (chan->channel == 4)
+               *val = 103;
+            else if (chan->channel == 5)
+               *val = 112;
+            return IIO_VAL_INT;
 
+      default: 
+            return -EINVAL;
+    }
+}
+
+ static int iio_adc_cora_write_raw(struct iio_dev *indio_dev, 
+                              struct iio_chan_spec const *chan, 
+                              int val, 
+                              int val2, 
+                              long mask)      
+{
+   switch(mask) {
+      case IIO_CHAN_INFO_RAW:
+            if (chan->channel == 0)
+               dev_info(&indio_dev->dev, "Trying to write to channel 0: %d\n", val);
+            else if (chan->channel == 1)
+               dev_info(&indio_dev->dev, "Trying to write to channel 1: %d\n", val);
+            else if (chan->channel == 2)
+               dev_info(&indio_dev->dev, "Trying to write to channel 2: %d\n", val);
+            else if (chan->channel == 3)
+               dev_info(&indio_dev->dev, "Trying to write to channel 3: %d\n", val);
+            else if (chan->channel == 4)
+               dev_info(&indio_dev->dev, "Trying to write to channel 4: %d\n", val);
+            else if (chan->channel == 5)
+               dev_info(&indio_dev->dev, "Trying to write to channel 5: %d\n", val);
+            return 0;
+
+      default:
+         return -EINVAL; 
+   }
+}
+
+ static const struct iio_chan_spec iio_adc_cora_channels[] = {
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 0,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   },
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 1,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   },
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 2,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   },
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 3,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   },
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 4,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   },
+   {
+      .type = IIO_VOLTAGE,
+      .channel = 5,
+      .indexed = 1,
+      .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+   }
+ };
+
+  static const struct iio_info iio_adc_cora_info = {
+   .read_raw = &iio_adc_cora_read_raw,
+   .write_raw = &iio_adc_cora_write_raw
  };
 
  static int iio_adc_probe(struct spi_device *spi){
@@ -20,7 +114,9 @@
     indio_dev = devm_iio_device_alloc(&spi->dev, 0);
 
     indio_dev->name = "ad5592r_s";
-    indio_dev->info = &iio_adc_info;
+    indio_dev->info = &iio_adc_cora_info;
+    indio_dev->channels = iio_adc_cora_channels;
+    indio_dev->num_channels = 6;
 
     return devm_iio_device_register(&spi->dev, indio_dev);
  }
