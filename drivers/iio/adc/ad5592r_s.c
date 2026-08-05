ba6dd25950f0175367fdd80452a0ce6049870966
Maya Mascasan <mascasan.se.maya@student.utcluj.ro>
2026-08-05 12:20:49 +0300

    drivers: iio: adc: Implemented the version for the ad5592r_s with 6 channels
    - Added the necessary functions for read/write taking into account only the 6 channels needed, instead of the 8 that are possible
    - Implemented a simple functionality which reads and sets the *val parameter with the channel number, for now.
    - Implemented a simple functionality which writes in the terminal (visible after the command 'dmesg') which channel is active, out of the 6 possible.
    
    Signed-off-by: Maya Mascasan <mascasan.se.maya@student.utcluj.ro>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -10,7 +10,100 @@
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
 
-static const struct iio_info iio_ad5592r_s_info = {};
+const int CHANNEL_0 = 0;
+const int CHANNEL_1 = 1;
+const int CHANNEL_2 = 2;
+const int CHANNEL_3 = 3;
+const int CHANNEL_4 = 4;
+const int CHANNEL_5 = 5;
+
+static int iio_ad5592r_s_read_raw(struct iio_dev *indio_dev, struct iio_chan_spec const *chan, int *val, int *val2, long mask)
+{
+    switch(mask){
+        case IIO_CHAN_INFO_RAW:
+            if(chan->channel == CHANNEL_0) 
+                *val = 0;
+            else if(chan->channel == CHANNEL_1) 
+                *val = 1;
+                else if(chan->channel == CHANNEL_2)
+                    *val = 2;
+                    else if(chan->channel == CHANNEL_3)
+                        *val = 3;
+                        else if(chan->channel == CHANNEL_4)
+                            *val = 4;
+                            else if(chan->channel == CHANNEL_5)
+                                *val = 5;    
+            return IIO_VAL_INT;                
+        default: 
+            return -EINVAL;
+    }
+}
+
+static int iio_ad5592r_s_write_raw(struct iio_dev *indio_dev, struct iio_chan_spec const *chan, int val, int val2, long mask)
+{
+    switch(mask){
+        case IIO_CHAN_INFO_RAW:
+            if(chan->channel == CHANNEL_0) 
+                dev_info(&indio_dev->dev, "Trying to write to channel 0: %d", val);
+            else  if(chan->channel == CHANNEL_1) 
+                    dev_info(&indio_dev->dev, "Trying to write to channel 1: %d", val);
+                else  if(chan->channel == CHANNEL_2)
+                        dev_info(&indio_dev->dev, "Trying to write to channel 2: %d", val);  
+                    else if(chan->channel == CHANNEL_3)
+                            dev_info(&indio_dev->dev, "Trying to write to channel 3: %d", val);
+                        else if(chan->channel == CHANNEL_4)
+                                dev_info(&indio_dev->dev, "Trying to write to channel 4: %d", val); 
+                            else if(chan->channel == CHANNEL_5)
+                                    dev_info(&indio_dev->dev, "Trying to write to channel 5: %d", val); 
+            return 0;                
+        default: 
+            return -EINVAL;
+    }
+}
+static const struct iio_chan_spec iio_ad5592r_s_channels[] = {
+    {
+        .type = IIO_VOLTAGE,
+        .channel = CHANNEL_0,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .channel = CHANNEL_1,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .channel = CHANNEL_2,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .channel = CHANNEL_3,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .channel = CHANNEL_4,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .channel = CHANNEL_5,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW)
+    }
+};
+
+
+static const struct iio_info iio_ad5592r_s_info = {
+    .read_raw = &iio_ad5592r_s_read_raw,
+    .write_raw = &iio_ad5592r_s_write_raw
+};
 
 // probe function
 static int iio_ad5592r_s_probe(struct spi_device *spi){
@@ -18,6 +111,8 @@ static int iio_ad5592r_s_probe(struct spi_device *spi){
     indio_dev = devm_iio_device_alloc(&spi->dev,0);
     indio_dev->name = "ad5592r_s";
     indio_dev->info = &iio_ad5592r_s_info;
+     indio_dev->channels = iio_ad5592r_s_channels;
+    indio_dev->num_channels = 6;
 
     return devm_iio_device_register(&spi->dev, indio_dev);
 } 
@@ -30,9 +125,6 @@ static struct spi_driver iio_ad5592r_s_driver = {
     .probe = iio_ad5592r_s_probe
 };
 module_spi_driver(iio_ad5592r_s_driver);
-
-
-
 
 
 MODULE_AUTHOR("Mascasan Maya-Lorena <mascasan.se.maya@student.utcluj.ro>");
