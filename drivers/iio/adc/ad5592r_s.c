a80b5a8b38101d6389d7b6b182507e8b2dc786a7
Denisa Dersedan <denisa.dersedan@analog.com>
2026-08-05 12:27:34 +0300

    drivers/iio/adc: Added channels to ADC driver implementation
    - Added the two channels of the ADC.
    - Created read/write functions for these channels according to the type of input/output (specified by the bitmask).
    - Connected the newly added channels to the device and the newly added functions to the iio_info function.
    
    Signed-off-by: Denisa Dersedan <denisa.dersedan@analog.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -2,7 +2,122 @@
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
 
+static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
+                    struct iio_chan_spec const *chan,
+                    int *val,
+                    int *val2,
+                    long mask)
+{
+    switch (mask) {
+        case IIO_CHAN_INFO_RAW:
+            switch (chan->channel) {
+                case 0:
+                    *val = 0;
+                    break;
+                case 1:
+                    *val = 1;
+                    break;
+                case 2:
+                    *val = 2;
+                    break;
+                case 3:
+                    *val = 3;
+                    break;
+                case 4:
+                    *val = 4;
+                    break;
+                case 5:
+                    *val = 5;
+                    break;
+                default:
+                    *val = 6;
+                    break;
+            }
+            return IIO_VAL_INT;
+        default:
+            return -EINVAL;
+    }
+}
+
+static int ad5592r_s_write_raw(struct iio_dev *indio_dev,
+                    struct iio_chan_spec const *chan,
+                    int val,
+                    int val2,
+                    long mask)
+{
+    switch (mask) {
+        case IIO_CHAN_INFO_RAW:
+            switch(chan->channel) {
+                case 0:
+                    dev_info(&indio_dev->dev, "Trying to write to channel 0");
+                    break;
+                case 1:
+                    dev_info(&indio_dev->dev, "Trying to write to channel 1");
+                    break;
+                case 2:
+                    dev_info(&indio_dev->dev, "Trying to write to channel 2");
+                    break;
+                case 3:
+                    dev_info(&indio_dev->dev, "Trying to write to channel 3");
+                    break;
+                case 4:
+                    dev_info(&indio_dev->dev, "Trying to write to channel 4");
+                    break;
+                case 5:
+                    dev_info(&indio_dev->dev, "Trying to write to channel 5");
+                    break;
+                default:
+                    dev_info(&indio_dev->dev, "Reached default");
+                    break;
+            }
+            return 0;
+        default:
+            return -EINVAL;
+    }
+}
+
+static const struct iio_chan_spec ad5592r_s_channels[] = {
+    {
+        .type = IIO_VOLTAGE,
+        .channel = 0,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }, 
+    {
+        .type = IIO_VOLTAGE,
+        .channel = 1,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .channel = 2,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }, 
+    {
+        .type = IIO_VOLTAGE,
+        .channel = 3,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .channel = 4,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }, 
+    {
+        .type = IIO_VOLTAGE,
+        .channel = 5,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }
+};
+
 static const struct iio_info ad5592rs_info = {
+    .read_raw = &ad5592r_s_read_raw,
+    .write_raw = &ad5592r_s_write_raw
 };
 
 static int ad5592rs_probe(struct spi_device *spi)
@@ -12,6 +127,9 @@ static int ad5592rs_probe(struct spi_device *spi)
 
     indio_dev->name = "ad5592r_s";
     indio_dev->info = &ad5592rs_info;
+    indio_dev->channels = ad5592r_s_channels;
+    indio_dev->num_channels = 6;
+
     return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
