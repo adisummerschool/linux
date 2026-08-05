b422b96f2f1f1f63a733b4cc736be24e3e10d06c
Papp Richard <pappr805@gmail.com>
2026-08-05 11:00:52 +0300

    drivers/iio/adc: Implement 2 chanel read/write raw data for emulator
    Add functions for reading/writing raw data. Hardcode values to test
    functionality
    adc/iio_adc_emu.c: Add iio_adc_emu_read/write_raw.
    Take pointers to chanel and indio_dev typ structs.
    Determine which chanel is read, and written to with a switch-case
    statement. On default return invalid value error.
    
    Signed-off-by: Papp Richard <pappr805@gmail.com>

--- drivers/iio/adc/iio_adc_emu.c
+++ drivers/iio/adc/iio_adc_emu.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
- * AD7170/AD7171 and AD7780/AD7781 SPI ADC driver
+ * AD5592r SPI ADC driver emulator
  *
  * Copyright 2011 Analog Devices Inc.
  * Copyright 2019 Renato Lui Geh
@@ -10,8 +10,61 @@
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
 
-static const struct iio_info iio_adc_emu_info = {
 
+static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
+			struct iio_chan_spec const *chan,
+			int *val,
+			int *val2,
+			long mask)
+{
+    switch(mask){
+        case IIO_CHAN_INFO_RAW:
+            if (chan->channel)
+                *val = 67;
+            else 
+                *val = 76;
+            return IIO_VAL_INT;
+        default:
+            return -EINVAL;
+    }
+}
+
+static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,
+			 struct iio_chan_spec const *chan,
+			 int val,
+			 int val2,
+			 long mask)
+{
+    switch(mask){
+        case IIO_CHAN_INFO_RAW:
+            if (chan->channel)
+                dev_info(&indio_dev->dev,"Trying to write to chanel 0");
+            else 
+                dev_info(&indio_dev->dev,"Trying to write to chanel 1");
+            return 0;
+        default:
+            return -EINVAL;
+    }
+}
+
+static const struct iio_chan_spec iio_adc_emu_chanels[] = {
+    {
+        .type = IIO_VOLTAGE,
+        .channel = 0,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    },
+    {
+        .type = IIO_VOLTAGE,
+        .channel = 1,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+    }
+};
+
+static const struct iio_info iio_adc_emu_info = {
+    .read_raw = &iio_adc_emu_read_raw,
+    .write_raw = &iio_adc_emu_write_raw,
 };
 
 
@@ -22,6 +75,9 @@ static int iio_adc_emu_probe(struct spi_device *spi){
     
     indio_dev->name ="iio_adc_emu";
     indio_dev->info = &iio_adc_emu_info;
+
+    indio_dev->channels = iio_adc_emu_chanels;
+    indio_dev->num_channels = 2;
     
     return devm_iio_device_register(&spi->dev,indio_dev);
 
