d1ffc3a2837e2669988670802378434198f5db2a
Pelin Mihai <mihai.pelin01@gmail.com>
2026-08-05 12:17:27 +0300

    drivers: iio: adc: add basic IIO channel support
    Implement basic IIO channel support by:
    - adding read_raw() and write_raw() callbacks
    - defining six voltage channels
    - registering IIO device information
    
    Signed-off-by: Pelin Mihai <mihai.pelin01@gmail.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -9,9 +9,124 @@
  #include <linux/spi/spi.h>
  #include <linux/iio/iio.h>
 
+ static int ad5592r_s_read_raw(struct iio_dev *indio_dev, //folosim pentru a citi datele de la driver
+ 				struct iio_chan_spec const *chan,
+ 				int *val, //returnam valarea prin referinta pentru a putea fi modificata in functie de ce citim
+				int *val2, //folosim pentru floating point
+				long mask)
+ {
+ 	switch (mask) {
+ 	case IIO_CHAN_INFO_RAW:
+
+	switch (chan->channel) {
+		case 0:
+			*val = 67;
+			break;
+		case 1:
+			*val = 76;
+			break;
+		case 2:
+			*val = 45;
+			break;
+		case 3:
+			*val = 23;
+			break;
+		case 4:			
+			*val = 12;
+			break;
+		case 5:
+			*val = 34;
+			break;
+		default:
+			return -EINVAL;
+	}
+		return IIO_VAL_INT;	
+
+ 	default:
+ 		return -EINVAL;
+ 	}
+ }
+
+  static int ad5592r_s_write_raw(struct iio_dev *indio_dev, //device(ar trebui sa fie canalul nostru), canalul, valoarea, valoarea2, masca
+ 				struct iio_chan_spec const *chan,
+ 				int val,
+ 				int val2,
+ 				long mask)
+ {
+		switch (mask) {
+			case IIO_CHAN_INFO_RAW:
+				switch (chan->channel) {
+				case 0:
+					dev_info(&indio_dev->dev, "Trying to write to channel 0\n");
+					break;
+				case 1:
+					dev_info(&indio_dev->dev, "Trying to write to channel 1\n");
+					break;
+				case 2:
+					dev_info(&indio_dev->dev, "Trying to write to channel 2\n");
+					break;
+				case 3:
+					dev_info(&indio_dev->dev, "Trying to write to channel 3\n");
+					break;
+				case 4:
+					dev_info(&indio_dev->dev, "Trying to write to channel 4\n");
+					break;
+				case 5:
+					dev_info(&indio_dev->dev, "Trying to write to channel 5\n");
+					break;
+				default:
+					return -EINVAL;
+				}
+				return 0;
+			default:
+				return -EINVAL;
+			}
+ }
+
+
+  static const struct iio_chan_spec ad5592r_s_channels[] = { //confirurare canalelor, in cazul nostru 2 canale de tip tensiune
+ 	{
+ 		.type = IIO_VOLTAGE,
+ 		.indexed = 1,
+ 		.channel = 0,
+ 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+ 	},
+ 	{
+ 		.type = IIO_VOLTAGE,
+ 		.indexed = 1,
+ 		.channel = 1,
+ 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+ 	},
+	{
+ 		.type = IIO_VOLTAGE,
+ 		.indexed = 1,
+ 		.channel = 2,
+ 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+ 	},
+ 	{
+ 		.type = IIO_VOLTAGE,
+ 		.indexed = 1,
+ 		.channel = 3,
+ 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+ 	},
+	{
+ 		.type = IIO_VOLTAGE,
+ 		.indexed = 1,
+ 		.channel = 4,
+ 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+ 	},
+ 	{
+ 		.type = IIO_VOLTAGE,
+ 		.indexed = 1,
+ 		.channel = 5,
+ 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+ 	},
+ };
+
 
  static const struct iio_info ad5592r_s_info = {
-
+	.read_raw = &ad5592r_s_read_raw,
+	.write_raw = &ad5592r_s_write_raw,
  };
 
 
@@ -25,6 +140,8 @@
 
     indio_dev->name = "ad5592r_s";
     indio_dev->info = &ad5592r_s_info;
+	indio_dev->channels = ad5592r_s_channels;
+	indio_dev->num_channels = 6;
 
  	return devm_iio_device_register(&spi->dev, indio_dev);
  }
