a0be6b602fb7a9758f6bf33c83c13e103625d112
Molnar Levente <mlevente388@gmail.com>
2026-08-05 12:40:48 +0300

    embeded/adc : Added ADC driver compatibility for its channels
    -Added reading/writing posibility for AD5592R(_S)
    -Hotfixed an earlier bug where the device was disabled(EARLIER COMMITS HAD A LIE WHERE EVERYTHING WAS WORKING)
    -ad5592r_s is seen corectly on SSH (fixed devicetree)
    -6 channels added (X-2,Y-2,Z-2 poz)
    
    Signed-off-by: Molnar Levente <mlevente388@gmail.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -10,7 +10,126 @@
  #include <linux/iio/iio.h>
 
 
-  static const struct iio_info adc_ad5592_driver_info ={};
+
+static int iio_adc_read_raw(struct iio_dev *indio_dev,struct iio_chan_spec const *chan,int *val,int *val2,long mask)
+    {
+        switch (mask) 
+            {
+                case IIO_CHAN_INFO_RAW:
+                    switch (chan->channel)
+                    {
+                        case 0:
+                            *val = 0;
+                            break;
+                        case 1:
+                            *val = 1;
+                            break;
+                        case 2:
+                            *val = 2;
+                            break;
+                        case 3:
+                            *val = 3;
+                            break;
+                        case 4:
+                            *val = 4;
+                            break;
+                        case 5:
+                            *val = 5;
+                            break;
+                        default:
+                            *val=404;
+                    }
+                    return  IIO_VAL_INT;      
+                default:
+                    return -EINVAL;
+            }
+    }
+ 
+static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec const *chan, int val, int val2, long mask)
+    {
+        switch(mask)
+        {
+            case IIO_CHAN_INFO_RAW:
+                switch(chan->channel)
+                {
+                    case 0:
+                        dev_info(&indio_dev->dev,"Trying to write to channel 0"); 
+                        break; 
+                    case 1:
+                        dev_info(&indio_dev->dev,"Trying to write to channel 1");  
+                        break;
+                    case 2:
+                        dev_info(&indio_dev->dev,"Trying to write to channel 2"); 
+                        break; 
+                    case 3:
+                        dev_info(&indio_dev->dev,"Trying to write to channel 3");  
+                        break;
+                    case 4:
+                        dev_info(&indio_dev->dev,"Trying to write to channel 4");
+                        break;  
+                    case 5:
+                        dev_info(&indio_dev->dev,"Trying to write to channel 5"); 
+                        break; 
+                    default:
+                        dev_info(&indio_dev->dev,"default reached"); 
+                        break; 
+                } 
+                return  0;    
+            default:
+                return -EINVAL;     
+        }    
+    }
+
+
+  static const struct iio_chan_spec iio_adc_channels[]={
+    {
+        .type =IIO_VOLTAGE,
+        .channel= 0,
+        .indexed= 1,
+        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+    },
+
+    {
+        .type =IIO_VOLTAGE,
+        .channel= 1,
+        .indexed= 1,
+        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+    },
+
+    {
+        .type =IIO_VOLTAGE,
+        .channel= 2,
+        .indexed= 1,
+        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+    },
+
+    {
+        .type =IIO_VOLTAGE,
+        .channel= 3,
+        .indexed= 1,
+        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+    },
+
+    {
+        .type =IIO_VOLTAGE,
+        .channel= 4,
+        .indexed= 1,
+        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+    },
+
+    {
+        .type =IIO_VOLTAGE,
+        .channel= 5,
+        .indexed= 1,
+        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+    }
+  };
+
+
+static const struct iio_info adc_ad5592_driver_info ={
+        .read_raw= &iio_adc_read_raw,
+        .write_raw= &iio_adc_write_raw,
+  };
 
 
 //in linux se face prin static ca in acest fisier sa fie chemat exact functia asta insine
@@ -22,6 +141,10 @@
 
         indio_dev->name="ad5592r_s";
         indio_dev->info=&adc_ad5592_driver_info;
+
+        indio_dev->channels=iio_adc_channels;
+        indio_dev->num_channels=6;
+
         return devm_iio_device_register(&spi->dev,indio_dev);
         
     }
