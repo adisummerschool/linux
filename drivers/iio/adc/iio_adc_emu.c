bd65d4affdf326919454f116dd724b8a0209cd09
Cosmin Strava <kosmin.strava@gmail.com>
2026-08-06 11:06:50 +0300

    drivers: iio: adc: add enable functionality and loopback
    Add support for enabling and disabling raw channel access.
    Store written values for both ADC channels and return them on
    subsequent reads to provide basic loopback functionality.
    
    Signed-off-by: Cosmin Strava <kosmin.strava@gmail.com>

--- drivers/iio/adc/iio_adc_emu.c
+++ drivers/iio/adc/iio_adc_emu.c
@@ -9,38 +9,69 @@
  #include <linux/spi/spi.h>
  #include <linux/iio/iio.h>
 
+ struct iio_adc_emu_st{
+	 int reg_select;
+	 int chan_val[2];
+ };
+
 static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
 			       struct iio_chan_spec const *chan,
 			       int *val,
 				   int *val2,
 				   long mask)
 {
+	struct iio_adc_emu_st *st = iio_priv(indio_dev);
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-		if(chan->channel)
-			*val = 69;
+		if(!st->reg_select){
+			if(chan->channel)
+				*val = st->chan_val[1];
+			else
+				*val = st->chan_val[0];
+			return IIO_VAL_INT;
+		}
 		else
-			*val = 42;
-		
+			return -EINVAL;
+
+	case IIO_CHAN_INFO_ENABLE:
+		*val = st->reg_select;
 		return IIO_VAL_INT;
+
 	default:
 		return -EINVAL;
 	}
 }
 
 static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,
-				struct iio_chan_spec const *chan,
-				int val,
-				int val2,
-				long mask)
+				 const struct iio_chan_spec *chan,
+				 int val,
+				 int val2,
+				 long mask)
 {
+	struct iio_adc_emu_st *st = iio_priv(indio_dev);
+
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-		if(chan->channel)
-			dev_info(&indio_dev->dev, "Try to write tochannel 1\n");
-		else
-			dev_info(&indio_dev->dev, "Try to write to channel 0\n");
+		if (!st->reg_select) {
+			if (chan->channel) {
+				dev_info(&indio_dev->dev,
+					 "Try to write to channel 1\n");
+				st->chan_val[1] = val;
+			} else {
+				dev_info(&indio_dev->dev,
+					 "Try to write to channel 0\n");
+				st->chan_val[0] = val;
+			}
+
+			return 0;
+		}
+
+		return -EINVAL;
+
+	case IIO_CHAN_INFO_ENABLE:
+		st->reg_select = val ? 1 : 0;
 		return 0;
+
 	default:
 		return -EINVAL;
 	}
@@ -52,12 +83,14 @@ static const struct iio_chan_spec iio_adc_emu_channels[] = {
 		.indexed = 1,
 		.channel = 0,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 	{
 		.type = IIO_VOLTAGE,
 		.indexed = 1,
 		.channel = 1,
-		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),  
+		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	}
 };
 
@@ -69,13 +102,17 @@ static const struct iio_info iio_adc_emu_info = {
 
  static int iio_adc_emu_probe(struct spi_device *spi) {
  	struct iio_dev *indio_dev;
+	struct iio_adc_emu_st *st;
 
-    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+    indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 
+	st = iio_priv(indio_dev);
+	st->reg_select = 1; // logica inversa
+	memset(st->chan_val, 0, sizeof(st->chan_val));
     indio_dev->name = "iio_adc_emu";
     indio_dev->info = &iio_adc_emu_info;
 	indio_dev->channels = iio_adc_emu_channels;
-	indio_dev->num_channels = 2;
+	indio_dev->num_channels = ARRAY_SIZE(iio_adc_emu_channels);
 
     return devm_iio_device_register(&spi->dev, indio_dev);
 
