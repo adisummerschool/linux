49ba8535b09314ac0ffa6c324257efe0ffcc0a88
Molnar Levente <mlevente388@gmail.com>
2026-08-06 15:08:48 +0300

    embeded/adc : AD5592R(_S) drivers for Coraz7s update
    -Added writing values into channels functionality
    -Added an enable functionality which stops reading and writing to the channels.
    
    Signed-off-by: Molnar Levente <mlevente388@gmail.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -9,37 +9,52 @@
  #include <linux/spi/spi.h>
  #include <linux/iio/iio.h>
 
-
+struct iio_adc_st_prv 
+    {
+        int reg_select;
+        int chan_val[6];
+    };
 
 static int iio_adc_read_raw(struct iio_dev *indio_dev,struct iio_chan_spec const *chan,int *val,int *val2,long mask)
     {
+        struct iio_adc_st_prv *st = iio_priv(indio_dev);
         switch (mask) 
             {
                 case IIO_CHAN_INFO_RAW:
+                    if(!st->reg_select)
+                    {
                     switch (chan->channel)
                     {
                         case 0:
-                            *val = 0;
+                            *val = st->chan_val[0];
                             break;
                         case 1:
-                            *val = 1;
+                            *val = st->chan_val[1];
                             break;
                         case 2:
-                            *val = 2;
+                            *val = st->chan_val[2];
                             break;
                         case 3:
-                            *val = 3;
+                            *val = st->chan_val[3];
                             break;
                         case 4:
-                            *val = 4;
+                            *val = st->chan_val[4];
                             break;
                         case 5:
-                            *val = 5;
+                            *val = st->chan_val[5];
                             break;
                         default:
                             *val=404;
                     }
-                    return  IIO_VAL_INT;      
+                    return  IIO_VAL_INT;   
+                    }
+                    else
+                    {
+                        return -EINVAL;
+                    }
+                case IIO_CHAN_INFO_ENABLE:
+                    *val=st->reg_select;
+                    return IIO_VAL_INT;
                 default:
                     return -EINVAL;
             }
@@ -47,34 +62,52 @@ static int iio_adc_read_raw(struct iio_dev *indio_dev,struct iio_chan_spec const
  
 static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec const *chan, int val, int val2, long mask)
     {
+        struct iio_adc_st_prv *st = iio_priv(indio_dev);
         switch(mask)
         {
             case IIO_CHAN_INFO_RAW:
-                switch(chan->channel)
-                {
-                    case 0:
-                        dev_info(&indio_dev->dev,"Trying to write to channel 0"); 
-                        break; 
-                    case 1:
-                        dev_info(&indio_dev->dev,"Trying to write to channel 1");  
-                        break;
-                    case 2:
-                        dev_info(&indio_dev->dev,"Trying to write to channel 2"); 
-                        break; 
-                    case 3:
-                        dev_info(&indio_dev->dev,"Trying to write to channel 3");  
-                        break;
-                    case 4:
-                        dev_info(&indio_dev->dev,"Trying to write to channel 4");
-                        break;  
-                    case 5:
-                        dev_info(&indio_dev->dev,"Trying to write to channel 5"); 
-                        break; 
-                    default:
-                        dev_info(&indio_dev->dev,"default reached"); 
-                        break; 
-                } 
-                return  0;    
+                if(!st->reg_select)
+                    {
+                    switch(chan->channel)
+                        {
+                            case 0:
+                                dev_info(&indio_dev->dev,"Trying to write to channel 0"); 
+                                st->chan_val[0]=val;
+                                break; 
+                            case 1:
+                                dev_info(&indio_dev->dev,"Trying to write to channel 1");
+                                st->chan_val[1]=val;  
+                                break;
+                            case 2:
+                                dev_info(&indio_dev->dev,"Trying to write to channel 2");
+                                st->chan_val[2]=val; 
+                                break; 
+                            case 3:
+                                dev_info(&indio_dev->dev,"Trying to write to channel 3");
+                                st->chan_val[3]=val;  
+                                break;
+                            case 4:
+                                dev_info(&indio_dev->dev,"Trying to write to channel 4");
+                                st->chan_val[4]=val;
+                                break;  
+                            case 5:
+                                dev_info(&indio_dev->dev,"Trying to write to channel 5"); 
+                                st->chan_val[5]=val;
+                                break; 
+                            default:
+                                dev_info(&indio_dev->dev,"default reached"); 
+                                st->chan_val[1]=404;
+                                break;  
+                        }
+                        return  0; 
+                    } 
+                else
+                    return -EINVAL; 
+
+            case IIO_CHAN_INFO_ENABLE:
+                st->reg_select = val ? 1:0;
+                return 0; 
+
             default:
                 return -EINVAL;     
         }    
@@ -87,6 +120,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .channel= 0,
         .indexed= 1,
         .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
     {
@@ -94,6 +128,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .channel= 1,
         .indexed= 1,
         .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
     {
@@ -101,6 +136,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .channel= 2,
         .indexed= 1,
         .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
     {
@@ -108,6 +144,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .channel= 3,
         .indexed= 1,
         .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
     {
@@ -115,6 +152,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .channel= 4,
         .indexed= 1,
         .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
     {
@@ -122,6 +160,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .channel= 5,
         .indexed= 1,
         .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     }
   };
 
@@ -136,14 +175,19 @@ static const struct iio_info adc_ad5592_driver_info ={
  static int ad5592r_s_probe(struct spi_device *spi)
     {
         struct iio_dev *indio_dev;
+        struct iio_adc_st_prv *st;
         
-        indio_dev = devm_iio_device_alloc(&spi->dev,0);
+        indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
+
+        st = iio_priv(indio_dev);
+        st->reg_select = 1;
+        memset(st->chan_val, 0 , sizeof(st->chan_val));
 
         indio_dev->name="ad5592r_s";
         indio_dev->info=&adc_ad5592_driver_info;
 
         indio_dev->channels=iio_adc_channels;
-        indio_dev->num_channels=6;
+        indio_dev->num_channels= ARRAY_SIZE(iio_adc_channels);
 
         return devm_iio_device_register(&spi->dev,indio_dev);
         
