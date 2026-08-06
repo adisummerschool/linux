71adbb76151a989d6e0078f93300b69f07a183bd
Denisa Dersedan <denisa.dersedan@analog.com>
2026-08-06 15:09:17 +0300

    drivers/iio/adc: Add enable feature and loopback for channels
    - Add a new structure which includes the new enable variable and some variables for the loopback for each of the channel.
    - Adjust code to include the newly added structure and the respective functionaly.
    
    Signed-off-by: Denisa Dersedan <denisa.dersedan@analog.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -2,37 +2,52 @@
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
 
+struct iio_ad5592r_s_st {
+        int reg_select;
+        int chan_val[6];
+};
+
 static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
-                    struct iio_chan_spec const *chan,
-                    int *val,
-                    int *val2,
-                    long mask)
+                struct iio_chan_spec const *chan,
+                int *val,
+                int *val2,
+                long mask)
 {
+    struct iio_ad5592r_s_st *st = iio_priv(indio_dev);
+
     switch (mask) {
         case IIO_CHAN_INFO_RAW:
-            switch (chan->channel) {
+            if (!st->reg_select) {
+                switch (chan->channel) {
                 case 0:
-                    *val = 0;
+                    *val = st->chan_val[0];
                     break;
                 case 1:
-                    *val = 1;
+                    *val = st->chan_val[1];
                     break;
                 case 2:
-                    *val = 2;
+                    *val = st->chan_val[2];
                     break;
                 case 3:
-                    *val = 3;
+                    *val = st->chan_val[3];
                     break;
                 case 4:
-                    *val = 4;
+                    *val = st->chan_val[4];
                     break;
                 case 5:
-                    *val = 5;
+                    *val = st->chan_val[5];
                     break;
                 default:
-                    *val = 6;
+                    *val = st->chan_val[6];
                     break;
+                }
+                return IIO_VAL_INT;
             }
+            else {
+                return -EINVAL;
+            }
+        case IIO_CHAN_INFO_ENABLE:
+            *val = st->reg_select;
             return IIO_VAL_INT;
         default:
             return -EINVAL;
@@ -40,104 +55,133 @@ static int ad5592r_s_read_raw(struct iio_dev *indio_dev,
 }
 
 static int ad5592r_s_write_raw(struct iio_dev *indio_dev,
-                    struct iio_chan_spec const *chan,
-                    int val,
-                    int val2,
-                    long mask)
+                struct iio_chan_spec const *chan,
+                int val,
+                int val2,
+                long mask)
 {
+    struct iio_ad5592r_s_st *st = iio_priv(indio_dev);
+
     switch (mask) {
-        case IIO_CHAN_INFO_RAW:
+    case IIO_CHAN_INFO_RAW:
+        if (!st->reg_select) {
             switch(chan->channel) {
-                case 0:
-                    dev_info(&indio_dev->dev, "Trying to write to channel 0");
-                    break;
-                case 1:
-                    dev_info(&indio_dev->dev, "Trying to write to channel 1");
-                    break;
-                case 2:
-                    dev_info(&indio_dev->dev, "Trying to write to channel 2");
-                    break;
-                case 3:
-                    dev_info(&indio_dev->dev, "Trying to write to channel 3");
-                    break;
-                case 4:
-                    dev_info(&indio_dev->dev, "Trying to write to channel 4");
-                    break;
-                case 5:
-                    dev_info(&indio_dev->dev, "Trying to write to channel 5");
-                    break;
-                default:
-                    dev_info(&indio_dev->dev, "Reached default");
-                    break;
+            case 0:
+                dev_info(&indio_dev->dev, "Trying to write to channel 0");
+                st->chan_val[0] = val;
+                break;
+            case 1:
+                dev_info(&indio_dev->dev, "Trying to write to channel 1");
+                st->chan_val[1] = val;
+                break;
+            case 2:
+                dev_info(&indio_dev->dev, "Trying to write to channel 2");
+                st->chan_val[2] = val;
+                break;
+            case 3:
+                dev_info(&indio_dev->dev, "Trying to write to channel 3");
+                st->chan_val[3] = val;
+                break;
+            case 4:
+                dev_info(&indio_dev->dev, "Trying to write to channel 4");
+                st->chan_val[4] = val;
+                break;
+            case 5:
+                dev_info(&indio_dev->dev, "Trying to write to channel 5");
+                st->chan_val[5] = val;
+                break;
+            default:
+                dev_info(&indio_dev->dev, "Reached default");
+                break;
             }
             return 0;
-        default:
+        }
+        else {
             return -EINVAL;
+        }
+    case IIO_CHAN_INFO_ENABLE:
+        st->reg_select = val ? 1 : 0;
+        return 0;
+    default:
+        return -EINVAL;
     }
 }
 
 static const struct iio_chan_spec ad5592r_s_channels[] = {
-    {
+        {
         .type = IIO_VOLTAGE,
         .channel = 0,
         .indexed = 1,
         .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-    }, 
-    {
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
+        }, 
+        {
         .type = IIO_VOLTAGE,
         .channel = 1,
         .indexed = 1,
         .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-    },
-    {
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
+        },
+        {
         .type = IIO_VOLTAGE,
         .channel = 2,
         .indexed = 1,
         .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-    }, 
-    {
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
+        }, 
+        {
         .type = IIO_VOLTAGE,
         .channel = 3,
         .indexed = 1,
         .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-    },
-    {
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
+        },
+        {
         .type = IIO_VOLTAGE,
         .channel = 4,
         .indexed = 1,
         .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-    }, 
-    {
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
+        }, 
+        {
         .type = IIO_VOLTAGE,
         .channel = 5,
         .indexed = 1,
         .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-    }
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
+        }
 };
 
 static const struct iio_info ad5592rs_info = {
-    .read_raw = &ad5592r_s_read_raw,
-    .write_raw = &ad5592r_s_write_raw
+        .read_raw = &ad5592r_s_read_raw,
+        .write_raw = &ad5592r_s_write_raw
 };
 
 static int ad5592rs_probe(struct spi_device *spi)
 {
-    struct iio_dev *indio_dev;
-    indio_dev = devm_iio_device_alloc(&spi->dev, 0);
+        struct iio_dev *indio_dev;
 
-    indio_dev->name = "ad5592r_s";
-    indio_dev->info = &ad5592rs_info;
-    indio_dev->channels = ad5592r_s_channels;
-    indio_dev->num_channels = 6;
+        struct iio_ad5592r_s_st *st;
 
-    return devm_iio_device_register(&spi->dev, indio_dev);
+        indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
+
+        st = iio_priv(indio_dev);
+        st->reg_select = 1;
+        memset(st->chan_val, 0, sizeof(st->chan_val));
+
+        indio_dev->name = "ad5592r_s";
+        indio_dev->info = &ad5592rs_info;
+        indio_dev->channels = ad5592r_s_channels;
+        indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_channels);
+
+        return devm_iio_device_register(&spi->dev, indio_dev);
 }
 
 static struct spi_driver ad5592rs_driver = {
-    .driver = {
-        .name = "ad5592r_s",
-    },
-    .probe = ad5592rs_probe,
+        .driver = {
+                .name = "ad5592r_s",
+        },
+        .probe = ad5592rs_probe,
 };
 module_spi_driver(ad5592rs_driver);
 
