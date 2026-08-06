20e3ae3dabcbfed1c086695492b7d4bafb460b9d
Molnar Levente <mlevente388@gmail.com>
2026-08-06 11:06:44 +0300

    embeded/emu : Updated emulation of AD5592R_S drivers
    -Added enable functionality(posibility of stopping a read or write of the channels)(can be toggled through ECHO)
    -Added custom writing to channels(can take values for the channels)
    -Fixed a typo when writing to the channel, the message was printing bad channel index
    
    -AMMENDS
    -Fixed some coding good practices
    Signed-off-by: Molnar Levente <mlevente388@gmail.com>

--- drivers/iio/adc/iio_adc_emu.c
+++ drivers/iio/adc/iio_adc_emu.c
@@ -9,17 +9,31 @@
  #include <linux/spi/spi.h>
  #include <linux/iio/iio.h>
 
+struct iio_adc_emu_st_prv 
+    {
+        int reg_select;
+        int chan_val[2];
+    };
 
 static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,struct iio_chan_spec const *chan,int *val,int *val2,long mask)
     {
+        struct iio_adc_emu_st_prv *st = iio_priv(indio_dev);
         switch (mask) 
             {
                 case IIO_CHAN_INFO_RAW:
-                    if(chan->channel)
-                        *val = 67;
+                    if(!st->reg_select)
+                        {
+                            if(chan->channel)
+                                *val = st->chan_val[1];
+                            else
+                                *val = st->chan_val[0];
+                            return  IIO_VAL_INT;
+                        }
                     else
-                        *val=76;
-                    return  IIO_VAL_INT;   
+                        return -EINVAL;
+                case IIO_CHAN_INFO_ENABLE:
+                    *val=st->reg_select;
+                    return IIO_VAL_INT;
                 default:
                     return -EINVAL;
             }
@@ -27,39 +41,55 @@ static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,struct iio_chan_spec c
  
 static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec const *chan, int val, int val2, long mask)
     {
+        struct iio_adc_emu_st_prv *st = iio_priv(indio_dev);
         switch(mask)
         {
             case IIO_CHAN_INFO_RAW:
-                if(chan->channel)
-                    dev_info(&indio_dev->dev,"Trying to write to channel 1");
-                else 
-                    dev_info(&indio_dev->dev,"Trying to write to channel 2");   
-                return 0;    
+            if(!st->reg_select)
+                {
+                    if(chan->channel)
+                        {
+                                dev_info(&indio_dev->dev, "Trying to write to channel 1");
+                                st->chan_val[1]=val;
+                        }
+                        
+                    else 
+                        {
+                                dev_info(&indio_dev->dev, "Trying to write to channel 0");
+                                st->chan_val[0] = val;
+                        }
+                    return 0; 
+                }
+            else
+                return -EINVAL;
+            case IIO_CHAN_INFO_ENABLE:
+                st->reg_select = val ? 1:0;
+                return 0; 
             default:
-                return -EINVAL;     
+                return -EINVAL;
         }    
     }
 
-  static const struct iio_chan_spec iio_adc_emu_channels[]={
+  static const struct iio_chan_spec iio_adc_emu_channels[] = {
     {
-        .type =IIO_VOLTAGE,
-        .channel= 0,
-        .indexed= 1,
-        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .type = IIO_VOLTAGE,
+        .channel = 0,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
     {
         .type =IIO_VOLTAGE,
-        .channel= 1,
-        .indexed= 1,
-        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .channel = 1,
+        .indexed = 1,
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
     },
-
   };
 
-  static const struct iio_info iio_adc_emu_info ={
-        .read_raw= &iio_adc_emu_read_raw,
-        .write_raw= &iio_adc_emu_write_raw,
+  static const struct iio_info iio_adc_emu_info = {
+        .read_raw = &iio_adc_emu_read_raw,
+        .write_raw = &iio_adc_emu_write_raw,
   };
 
 
@@ -67,30 +97,32 @@ static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec 
  static int iio_adc_probe(struct spi_device *spi)
     {
         struct iio_dev *indio_dev;
+        struct iio_adc_emu_st_prv *st;
         
-        indio_dev = devm_iio_device_alloc(&spi->dev,0);
+        indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 
-        indio_dev->name="iio_adc_emu";
-        indio_dev->info=&iio_adc_emu_info;
-        indio_dev->channels=iio_adc_emu_channels;
-        indio_dev->num_channels=2;
+        st = iio_priv(indio_dev);
+        st->reg_select = 1;
+        memset(st->chan_val, 0 , sizeof(st->chan_val));
+        indio_dev->name = "iio_adc_emu";
+        indio_dev->info = &iio_adc_emu_info;
 
-        
+        indio_dev->channels = iio_adc_emu_channels;
+        indio_dev->num_channels = ARRAY_SIZE(iio_adc_emu_channels);
 
-        return devm_iio_device_register(&spi->dev,indio_dev);
-        
+        return devm_iio_device_register(&spi->dev, indio_dev);
     }
 
  static struct spi_driver iio_adc_emu_driver={
         .driver={
-            .name="iio_driver"
+            .name = "iio_driver"
         },
         .probe = iio_adc_probe
     };
 
 
-module_spi_driver(iio_adc_emu_driver);
+        module_spi_driver(iio_adc_emu_driver);
 
-    MODULE_AUTHOR("Molnar Levente <mlevente388@gmail.com>");
-    MODULE_DESCRIPTION("IIO setup for and ADC5592-summer practice setup");
-    MODULE_LICENSE("GPL v2");
+        MODULE_AUTHOR("Molnar Levente <mlevente388@gmail.com>");
+        MODULE_DESCRIPTION("IIO setup for and ADC5592-summer practice setup");
+        MODULE_LICENSE("GPL v2");
