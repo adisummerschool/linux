d6508c02e73dc8af6e8cd1de3cf5520928c8108f
Alexandru Iordache <alexandru.iordache4@yahoo.com>
2026-08-11 12:38:21 +0300

    drivers: iio: adc: iio_emu_adc.c: Added register SPI communication.
    Added SPI read/write functions and wrapper for communication with
    ADC's registers.
    Added defines for better readability of SPI transfers.
    Linked SPI controller to ADC.
    
    Signed-off-by: Alexandru Iordache <alexandru.iordache4@yahoo.com>

--- drivers/iio/adc/iio_adc_emu.c
+++ drivers/iio/adc/iio_adc_emu.c
@@ -8,17 +8,98 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
+#include <linux/unaligned.h>
+#include <linux/bitfield.h>
+
+#define EMU_RDWR_MSK		BIT(7)
+#define EMU_ADDR_MSK		GENMASK(14, 8)
+#define EMU_DATA_MSK		GENMASK(7, 0)
+
+#define EMU_POWER_REG		0x02
+#define EMU_POWER_ENABLE	0x0
+#define EMU_POWER_DISABLE	0x20
 
 struct iio_adc_emu_st {
 	bool reg_select;
-        int chan_val[2];
+	int chan_val[2];
+	struct spi_device *spi;
 };
 
-static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
-				struct iio_chan_spec const *chan, int *val,
-				int *val2, long mask)
+static int iio_adc_emu_spi_read(struct iio_adc_emu_st *st, u8 addr, u8 *data)
+{
+	u8 tx = 0;
+	u8 rx = 0;
+	int ret = 0;
+	struct spi_transfer t[] = {
+		{
+			.tx_buf = &tx,
+			.len = 1,
+		},
+		{
+			.rx_buf = &rx,
+			.len = 1,
+		}
+	};
+
+	tx = FIELD_PREP(EMU_RDWR_MSK, 1) | addr;
+
+	ret = spi_sync_transfer(st->spi, t, 2);
+
+	if (ret) {
+		dev_info(&st->spi->dev,
+			"SPI READ transfer failed with error code %d\n",
+			ret);
+		return ret;
+	}
+
+	*data = rx;
+
+	return 0;
+}
+
+static int iio_adc_emu_spi_write(struct iio_adc_emu_st *st, u8 addr, u8 data)
+{
+	u16 tx = 0;
+	u16 payload = 0;
+	struct spi_transfer t = {
+		.tx_buf = &payload,
+		.len = 2,
+	};
+
+	tx = FIELD_PREP(EMU_RDWR_MSK, 0) |
+	     FIELD_PREP(EMU_ADDR_MSK, addr) |
+	     FIELD_PREP(EMU_DATA_MSK, data);
+
+	put_unaligned_be16(tx, &payload);
+
+	dev_info(&st->spi->dev, "constructed package: %x\n", payload);
+	dev_info(&st->spi->dev, "original tx: %x\n", tx);
+
+	return spi_sync_transfer(st->spi, &t, 1);
+}
+
+static int iio_adc_emu_debugfs_reg_access(struct iio_dev *indio_dev,
+					  unsigned int reg,
+					  unsigned int writeval,
+					  unsigned int *readval)
 {
 	struct iio_adc_emu_st *st = iio_priv(indio_dev);
+
+	if (readval) {
+		return iio_adc_emu_spi_read(st, reg, (u8 *)readval);
+	}
+
+	return iio_adc_emu_spi_write(st, reg, writeval);
+}
+
+static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
+				struct iio_chan_spec const *chan,
+				int *val,
+				int *val2,
+				long mask)
+{
+	struct iio_adc_emu_st *st = iio_priv(indio_dev);
+
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
 		if (!st->reg_select) {
@@ -31,40 +112,53 @@ static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
 		} else {
 			return -EINVAL;
 		}
-        
-        case IIO_CHAN_INFO_ENABLE:
-                *val = st->reg_select;
-                return IIO_VAL_INT;
+
+	case IIO_CHAN_INFO_ENABLE:
+		*val = st->reg_select;
+		return IIO_VAL_INT;
 	default:
 		return -EINVAL;
 	}
 }
 
 static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,
-				 struct iio_chan_spec const *chan, int val,
-				 int val2, long mask)
+				 struct iio_chan_spec const *chan,
+				 int val,
+				 int val2,
+				 long mask)
 {
 	struct iio_adc_emu_st *st = iio_priv(indio_dev);
 
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
-                if(!st->reg_select) {
-                        if (chan->channel) {
-			dev_info(&indio_dev->dev,
-				 "Trying to write to channel 1");
-                        st->chan_val[1] = val;
+		if (!st->reg_select) {
+			if (chan->channel) {
+				dev_info(&indio_dev->dev,
+					 "Trying to write to channel 1");
+				st->chan_val[1] = val;
+			} else {
+				dev_info(&indio_dev->dev,
+					 "Trying to write to channel 0");
+				st->chan_val[0] = val;
+			}
+			return 0;
 		} else {
-			dev_info(&indio_dev->dev,
-				 "Trying to write to channel 0");
-                        st->chan_val[0] = val;
+			return -EINVAL;
 		}
-		        return 0;
-                } else {
-                        return -EINVAL;
-                }
 	case IIO_CHAN_INFO_ENABLE:
-                st->reg_select = val ? 1 : 0;
-                return 0;
+		if (val) {
+			st->reg_select = 1;
+			iio_adc_emu_spi_write(st,
+					      EMU_POWER_REG,
+					      EMU_POWER_DISABLE);
+		} else {
+			st->reg_select = 0;
+			iio_adc_emu_spi_write(st,
+					      EMU_POWER_REG,
+					      EMU_POWER_ENABLE);
+		}
+
+		return 0;
 	default:
 		return -EINVAL;
 	}
@@ -76,20 +170,21 @@ static const struct iio_chan_spec iio_adc_emu_channels[] = {
 		.channel = 0,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-                .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	},
 	{
 		.type = IIO_VOLTAGE,
 		.channel = 1,
 		.indexed = 1,
 		.info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-                .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+		.info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
 	}
 };
 
 static const struct iio_info iio_adc_emu_info = {
 	.read_raw = &iio_adc_emu_read_raw,
 	.write_raw = &iio_adc_emu_write_raw,
+	.debugfs_reg_access = &iio_adc_emu_debugfs_reg_access,
 };
 
 static int iio_adc_emu_probe(struct spi_device *spi)
@@ -101,8 +196,8 @@ static int iio_adc_emu_probe(struct spi_device *spi)
 
 	st = iio_priv(indio_dev);
 	st->reg_select = 1;
-	memset(st->chan_val, 0,
-	       sizeof(sizeof(st->chan_val)));
+	st->spi = spi;
+	memset(st->chan_val, 0, sizeof(sizeof(st->chan_val)));
 
 	indio_dev->name = "iio_adc_emu";
 	indio_dev->info = &iio_adc_emu_info;
@@ -113,10 +208,10 @@ static int iio_adc_emu_probe(struct spi_device *spi)
 }
 
 static struct spi_driver iio_adc_emu_driver = {
-        .driver = {
-                .name = "iio_adc_emu",
-        },
-        .probe = iio_adc_emu_probe
+	.driver = {
+		.name = "iio_adc_emu",
+	},
+	.probe = iio_adc_emu_probe
 };
 module_spi_driver(iio_adc_emu_driver);
 
