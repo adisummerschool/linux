7d18c4c819ef269dafe83d6376bba365ad6f4847
Molnar Levente <mlevente388@gmail.com>
2026-08-11 17:54:34 +0300

    embedded/adc : Added reading functionality of the 6 channels of the AD5592R_S
    -Implemented reading functionality of the 6 IO channels of the AD5592R_S
    -Implemented configuration of channels and the AD5592R_S reading mode for ADC functionality
    
    Signed-off-by: Molnar Levente <mlevente388@gmail.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -9,11 +9,78 @@
  #include <linux/spi/spi.h>
  #include <linux/iio/iio.h>
 
+ #include <linux/unaligned.h>
+ #include <linux/bitfield.h>
+
+ #define AD5592R_S_MSB_MSK       BIT(15)
+ #define AD5592R_S_ADDR_MSK      GENMASK(14,11)
+ #define AD5592R_S_DATA_MSK      GENMASK(10,0)
+
+ #define AD5592R_S_ENBL_RDBK     BIT(6)
+ #define AD5592R_S_REG_RDBK_ADDR 0x7
+ #define AD5592R_S_REG_PD_ADDR   0xB
+ #define AD5592R_S_REG_ENBL_ADDR BIT(9)
+ #define AD5592R_S_REG_SLCT_RDBK GENMASK(5,2)
+
 struct iio_adc_st_prv 
     {
         int reg_select;
         int chan_val[6];
+        struct spi_device *spi;
     };
+static int ad5592r_s_spi_write(struct iio_adc_st_prv *st, u8 addr, u16 data)
+    {
+        u16 tx = 0;
+        u16 package = 0;
+        struct spi_transfer t = {
+            .tx_buf = &package,
+            .len = 2
+        };
+        tx = FIELD_PREP(AD5592R_S_MSB_MSK, 0) | FIELD_PREP(AD5592R_S_ADDR_MSK, addr) | FIELD_PREP(AD5592R_S_DATA_MSK, data);
+        put_unaligned_be16(tx, &package);
+        dev_info(&st->spi->dev,"Tx we constructed %x \n", tx);
+        dev_info(&st->spi->dev,"Package we constructed %x\n", package);
+        return spi_sync_transfer(st->spi, &t, 1);
+    }
+static int ad5592r_s_spi_read(struct iio_adc_st_prv *st, u8 addr, u16 *data)
+    {
+        u16 rx = 0;
+        u16 reg_db_data;
+        u16 rcv_data;
+        int ret = 0;
+        struct spi_transfer t = {
+            .tx_buf = NULL,
+            .rx_buf = &rx,
+            .len = 2
+        };
+    reg_db_data = FIELD_PREP(AD5592R_S_ENBL_RDBK, 1) | FIELD_PREP(AD5592R_S_REG_SLCT_RDBK, addr);
+    ret = ad5592r_s_spi_write(st, AD5592R_S_REG_RDBK_ADDR,reg_db_data);
+    if(ret)
+        {
+            dev_info(&st->spi->dev, "Writing the readback register failed");
+            return ret;
+        }
+    ret = spi_sync_transfer(st->spi, &t,1);   
+    if(ret)
+        {
+            dev_info(&st->spi->dev, "Failed receiveing readback");
+            return ret;
+        } 
+    rcv_data = get_unaligned_be16(&rx);
+    *data = FIELD_GET(AD5592R_S_DATA_MSK,rcv_data);
+    return 0;
+
+    }
+
+static int ad5592r_s_debugfs_reg_access(struct iio_dev *indio_dev, unsigned reg, unsigned writeval, unsigned *readval)
+    {
+        struct iio_adc_st_prv *st = iio_priv(indio_dev);
+        if(readval)
+            {
+                return ad5592r_s_spi_read(st,reg,(u16 *) readval);
+            }
+        return ad5592r_s_spi_write(st,reg,(u16 *) writeval);
+    }
 
 static int iio_adc_read_raw(struct iio_dev *indio_dev,struct iio_chan_spec const *chan,int *val,int *val2,long mask)
     {
@@ -72,31 +139,36 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
                         {
                             case 0:
                                 dev_info(&indio_dev->dev,"Trying to write to channel 0"); 
-                                st->chan_val[0]=val;
+                                st->chan_val[0] = val;
                                 break; 
                             case 1:
                                 dev_info(&indio_dev->dev,"Trying to write to channel 1");
-                                st->chan_val[1]=val;  
+                                st->chan_val[1] = val;  
                                 break;
                             case 2:
                                 dev_info(&indio_dev->dev,"Trying to write to channel 2");
-                                st->chan_val[2]=val; 
+                                st->chan_val[2] = val; 
                                 break; 
                             case 3:
                                 dev_info(&indio_dev->dev,"Trying to write to channel 3");
-                                st->chan_val[3]=val;  
+                                st->chan_val[3] = val;  
                                 break;
                             case 4:
                                 dev_info(&indio_dev->dev,"Trying to write to channel 4");
-                                st->chan_val[4]=val;
+                                st->chan_val[4] = val;
                                 break;  
                             case 5:
                                 dev_info(&indio_dev->dev,"Trying to write to channel 5"); 
-                                st->chan_val[5]=val;
+                                st->chan_val[5] = val;
                                 break; 
                             default:
                                 dev_info(&indio_dev->dev,"default reached"); 
-                                st->chan_val[1]=404;
+                                st->chan_val[0] = 404;
+                                st->chan_val[1] = 404;
+                                st->chan_val[2] = 404;
+                                st->chan_val[3] = 404;
+                                st->chan_val[4] = 404;
+                                st->chan_val[5] = 404;
                                 break;  
                         }
                         return  0; 
@@ -119,7 +191,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .type =IIO_VOLTAGE,
         .channel= 0,
         .indexed= 1,
-        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
         .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
@@ -127,7 +199,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .type =IIO_VOLTAGE,
         .channel= 1,
         .indexed= 1,
-        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
         .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
@@ -135,7 +207,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .type =IIO_VOLTAGE,
         .channel= 2,
         .indexed= 1,
-        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
         .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
@@ -143,7 +215,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .type =IIO_VOLTAGE,
         .channel= 3,
         .indexed= 1,
-        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
         .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
@@ -151,7 +223,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .type =IIO_VOLTAGE,
         .channel= 4,
         .indexed= 1,
-        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
         .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     },
 
@@ -159,7 +231,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
         .type =IIO_VOLTAGE,
         .channel= 5,
         .indexed= 1,
-        .info_mask_separate=BIT(IIO_CHAN_INFO_RAW),
+        .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
         .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
     }
   };
@@ -168,6 +240,7 @@ static int iio_adc_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec cons
 static const struct iio_info adc_ad5592_driver_info ={
         .read_raw= &iio_adc_read_raw,
         .write_raw= &iio_adc_write_raw,
+        .debugfs_reg_access = &ad5592r_s_debugfs_reg_access
   };
 
 
@@ -181,8 +254,10 @@ static const struct iio_info adc_ad5592_driver_info ={
 
         st = iio_priv(indio_dev);
         st->reg_select = 1;
+        st->spi=spi;
         memset(st->chan_val, 0 , sizeof(st->chan_val));
 
+        ad5592r_s_spi_write(st,AD5592R_S_REG_PD_ADDR,FIELD_PREP(AD5592R_S_REG_ENBL_ADDR,1));
         indio_dev->name="ad5592r_s";
         indio_dev->info=&adc_ad5592_driver_info;
 
