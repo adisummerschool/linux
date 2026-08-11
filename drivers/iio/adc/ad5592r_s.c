078bed39c12262c6a198fb583b88e6a1226d8441
Codrin Berinde <codrin.berinde@gmail.com>
2026-08-11 17:13:39 +0300

    drivers/iio/adc: implemented SPI communnication with AD5592r
    - implemented read and write SPI functions to/from registers
    on AD5592r analog-to-digital converter
    
    Signed-off-by: Codrin Berinde <codrin.berinde@gmail.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -1,11 +1,83 @@
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
+#include <linux/unaligned.h>
+#include <linux/bitfield.h>
+
+#define AD_CTRL_REG_MSB		BIT(15)
+#define AD_CTRL_REG_ADDR	GENMASK(14,11)
+#define AD_CTRL_REG_DATA	GENMASK(10,0)
+
+#define AD_REG_RD_MSB		BIT(15)
+#define AD_REG_RD_READ_REG	GENMASK(14,11)
+#define AD_REG_RD_EN		BIT(6)
+#define AD_REG_RD_ADDR		GENMASK(5,2)
+#define CONFIG_READ_AND_LDAC	0b0111
+#define REG_PD_ADDR		0xB
+#define AD_REG_EN_IREF		BIT(9)
 
 struct iio_ad5592r_s_st {
+	struct spi_device *spi;
 	int reg_select;
 	int chan_val[6];
 };
+
+static int iio_ad5592r_s_spi_write(struct iio_ad5592r_s_st *st, u8 addr, u16 data) {
+	u16 tx = 0;
+	u16 package = 0;
+	struct spi_transfer t = {
+		.tx_buf = &package,
+		.len = 2
+	};
+
+	tx = FIELD_PREP(AD_CTRL_REG_MSB, 0) | FIELD_PREP(AD_CTRL_REG_ADDR, addr)
+		| FIELD_PREP(AD_CTRL_REG_DATA, data);
+	put_unaligned_be16(tx, &package); //for suitable endianness
+	dev_info(&st->spi->dev, "tx we constructed 0x%x\n", tx);
+	dev_info(&st->spi->dev, "package we constructed 0x%x\n", package);
+
+	return spi_sync_transfer(st->spi, &t, 1);
+}
+
+static int iio_ad5592r_s_spi_read(struct iio_ad5592r_s_st *st, u8 addr, u16 *data) {
+	u16 rx = 0;
+	u16 tx_data = 0;
+	int ret = 0;
+
+	struct spi_transfer t = {
+		.tx_buf = NULL,
+		.rx_buf = &rx,
+		.len = 2
+	};
+
+	tx_data = FIELD_PREP(AD_REG_RD_EN, 1) 
+		| FIELD_PREP(AD_REG_RD_ADDR, addr);
+	ret = iio_ad5592r_s_spi_write(st, CONFIG_READ_AND_LDAC, tx_data);
+	if(ret) {
+		dev_info(&st->spi->dev, "Writing the readback register failed %d\n", ret);
+		return ret;
+	}
+
+	ret = spi_sync_transfer(st->spi, &t, 1);
+	if(ret) {
+		dev_info(&st->spi->dev, "SPI read transfer failed %d\n", ret);
+		return ret;
+	}
+
+	*data = FIELD_GET(AD_CTRL_REG_DATA, get_unaligned_be16(&rx));
+	return 0;
+}
+
+static int iio_ad5592r_s_debugfs_reg_access(struct iio_dev *indio_dev,
+					    unsigned reg, unsigned writeval,
+					    unsigned *readval)
+{
+	struct iio_ad5592r_s_st *st = iio_priv(indio_dev);
+	if(readval) //if we expect a value at this location, we read
+		return iio_ad5592r_s_spi_read(st, reg, (u16 *)readval);
+	// else we write
+	return iio_ad5592r_s_spi_write(st, reg, writeval);
+}
 
 static int iio_ad5592r_s_read_raw(struct iio_dev *indio_dev,
 				  struct iio_chan_spec const *chan, int *val,
@@ -152,7 +224,8 @@ static const struct iio_chan_spec iio_ad5592r_s_channels[] = {
 
 static const struct iio_info ad5592r_s_info = {
 	.read_raw = &iio_ad5592r_s_read_raw,
-	.write_raw = &iio_ad5592r_s_write_raw
+	.write_raw = &iio_ad5592r_s_write_raw,
+	.debugfs_reg_access = &iio_ad5592r_s_debugfs_reg_access
 };
 
 static int ad5592r_s_probe(struct spi_device *spi)
@@ -164,11 +237,15 @@ static int ad5592r_s_probe(struct spi_device *spi)
 
 	st = iio_priv(indio_dev);
 	st->reg_select = 1; //inverse logic, 1 = powered off
+	st->spi = spi;
+	
 	memset(st->chan_val, 0, sizeof(st->chan_val));
 	indio_dev->name = "ad5592r_s";
 	indio_dev->info = &ad5592r_s_info;
 	indio_dev->channels = iio_ad5592r_s_channels;
 	indio_dev->num_channels = ARRAY_SIZE(iio_ad5592r_s_channels);
+
+	iio_ad5592r_s_spi_write(st, REG_PD_ADDR, FIELD_PREP(AD_REG_EN_IREF, 1));
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
 }
