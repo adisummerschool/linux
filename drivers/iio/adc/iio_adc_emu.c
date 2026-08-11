6b2dd36c30a70402acef97ee65a35ab3379192e4
Tomescu Lucas <fanaticlucas98@gmail.com>
2026-08-11 11:56:02 +0300

    drivers: iio: adc: ad5592r_s: add device support and refactor driver
    Add SPI read and write support, debugfs register access,
    probe-time initialization, and IIO channel handling.
    
    Refactor the driver structure to support the AD5592R-S
    device and initialize the required register state.
    
    Signed-off-by: Tomescu Lucas <fanaticlucas98@gmail.com>

--- drivers/iio/adc/iio_adc_emu.c
+++ drivers/iio/adc/iio_adc_emu.c
@@ -5,14 +5,72 @@
  * Copyright 2026 Analog Devices Inc.
  */
 
+#include <linux/unaligned.h>
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
+#include <linux/bitfield.h>
+
+#define EMU_RDWR_MASK BIT(7)
+#define EMU_ADDR_MSK GENMASK(15, 8)
+#define EMU_DATA_MSK GENMASK(7, 0)
+#define EMU_POWER_REG 0x02
+#define EMU_POWER_ENABLE 0x0
+#define EMU_POWER_DISABLE 0x20
 
 struct iio_adc_emu_st {
+	struct spi_device *spi;
 	bool reg_select;
 	int chan_val[2];
 };
+
+static int iio_adc_emu_spi_read(struct iio_adc_emu_st *st, u8 addr, u8 *data)
+{
+	u8 tx = 0, rx = 0;
+	int ret = 0;
+	struct spi_transfer t[] = { { .tx_buf = &tx, .len = 1 },
+				    { .rx_buf = &rx, .len = 1 } };
+
+	tx = FIELD_PREP(EMU_RDWR_MASK, 1) | addr;
+
+	ret = spi_sync_transfer(st->spi, t, 2);
+	if (ret) {
+		dev_info(&st->spi->dev, "SPI read failed");
+		return ret;
+	}
+
+	*data = rx;
+	return 0;
+}
+
+static int iio_adc_emu_spi_write(struct iio_adc_emu_st *st, u8 addr, u8 data)
+{
+	u16 tx = 0;
+	u16 package = 0;
+	struct spi_transfer t = { .tx_buf = &package, .len = 2 };
+
+	tx = FIELD_PREP(EMU_RDWR_MASK, 0) | FIELD_PREP(EMU_ADDR_MSK, addr) |
+	     FIELD_PREP(EMU_DATA_MSK, data);
+	put_unaligned_be16(tx, &package);
+
+	dev_info(&st->spi->dev,
+		 "Constructed TX:0x%02x \nConstructed PACKAGE:0x%02x", tx,
+		 package);
+
+	return spi_sync_transfer(st->spi, &t, 1);
+}
+
+static int iio_adc_emu_debugfs_reg_access(struct iio_dev *indio_dev,
+					  unsigned reg, unsigned writeval,
+					  unsigned *readval)
+{
+	struct iio_adc_emu_st *st = iio_priv(indio_dev);
+
+	if (readval) {
+		return iio_adc_emu_spi_read(st, reg, (u8 *)readval);
+	}
+	return iio_adc_emu_spi_write(st, reg, writeval);
+}
 
 // structura pentru canalele ADC
 static const struct iio_chan_spec iio_adc_emu_channels[] = {
@@ -52,7 +110,7 @@ static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
 		}
 	case IIO_CHAN_INFO_ENABLE:
 		*val = st->reg_select ? 1 : 0;
-		return 0;
+		return IIO_VAL_INT;
 	default:
 		//returnam datele citite
 		return -EINVAL;
@@ -83,7 +141,14 @@ static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,
 			return -EINVAL;
 		}
 	case IIO_CHAN_INFO_ENABLE:
-		st->reg_select = val ? true : false;
+		if(val){
+		st->reg_select = 1;
+		iio_adc_emu_spi_write(st, EMU_POWER_REG, EMU_POWER_DISABLE);
+		}else{
+		st->reg_select = 0;
+		iio_adc_emu_spi_write(st, EMU_POWER_REG, EMU_POWER_ENABLE);
+		}
+
 		return 0;
 	default:
 		return -EINVAL;
@@ -93,7 +158,8 @@ static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,
 }
 static const struct iio_info iio_adc_emu_info = {
 	.read_raw = &iio_adc_emu_read_raw,
-	.write_raw = &iio_adc_emu_write_raw
+	.write_raw = &iio_adc_emu_write_raw,
+	.debugfs_reg_access = iio_adc_emu_debugfs_reg_access
 };
 // initializam driverul
 static int iio_adc_emu_probe(struct spi_device *spi)
@@ -106,6 +172,7 @@ static int iio_adc_emu_probe(struct spi_device *spi)
 
 	st = iio_priv(indio_dev);
 	st->reg_select = true;
+	st->spi = spi;
 	memset(st->chan_val, 0, sizeof(st->chan_val));
 	//popularea structurii
 	indio_dev->name = "iio_adc_emu";
