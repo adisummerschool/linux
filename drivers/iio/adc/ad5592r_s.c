465bf31368a4a6833fb14d0d7a73d6fbe784fd8f
Alexandru Iordache <alexandru.iordache4@yahoo.com>
2026-08-11 16:56:00 +0300

    drivers: iio: adc: ad5592r_s.c: Add register mapping via SPI to driver.
    Add bit masks according to AD5592R datasheet.
    Implement read/write to AD5592R's registers, using debugfs_reg_access
    with a wrapper ad5592r_s_debugfs_reg_access function.
    Added clock polarity to device tree.
    
    Signed-off-by: Alexandru Iordache <alexandru.iordache4@yahoo.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -2,17 +2,112 @@
 /*
  * AD5592R SPI ADC driver
  *
- * Copyright 2011 Analog Devices Inc.
+ * Copyright 2026 Analog Devices Inc.
  */
 
 #include <linux/module.h>
 #include <linux/spi/spi.h>
 #include <linux/iio/iio.h>
+#include <linux/unaligned.h>
+#include <linux/bitfield.h>
+
+#define AD5592R_S_REG_MSB_MSK			BIT(15)
+#define AD5592R_S_REG_ADDR_MSK			GENMASK(14, 11)
+#define AD5592R_S_REG_DATA_MSK			GENMASK(10, 0)
+
+#define AD5592R_S_REG_RDB_ADDR			0x7
+#define AD5592R_S_EN_READB			BIT(6)
+#define AD5592R_S_REG_RD			GENMASK(5, 2)
+
+#define AD5592R_S_ADC_REG			0x04
+#define AD5592R_S_ADC_ENABLE			0x3F
+
+#define AD5592R_S_PD_ADDR			0xB
+#define AD5592R_S_PD_IREF			BIT(9)
 
 struct adc_ad5592r_s_st {
 	bool reg_select;
 	int chan_val[6];
+	struct spi_device *spi;
+
 };
+
+static int ad5592r_s_spi_write(struct adc_ad5592r_s_st *st,
+				       u8 addr,
+				       u16 data)
+{
+	u16 tx = 0;
+	u16 payload = 0;
+	struct spi_transfer t = {
+		.tx_buf = &payload,
+		.len = 2,
+	};
+
+	tx = FIELD_PREP(AD5592R_S_REG_MSB_MSK, 0) |
+	     FIELD_PREP(AD5592R_S_REG_ADDR_MSK, addr) |
+	     FIELD_PREP(AD5592R_S_REG_DATA_MSK, data);
+
+	put_unaligned_be16(tx, &payload);
+
+	dev_info(&st->spi->dev, "SPI WRITE PAYLOAD: %x\n", payload);
+	dev_info(&st->spi->dev, "SPI WRITE TX: %x\n", tx);
+
+	return spi_sync_transfer(st->spi, &t, 1);
+}
+
+static int ad5592r_s_spi_read(struct adc_ad5592r_s_st *st, u8 addr, u16 *data)
+{
+	u16 rx = 0;
+	u16 reg_rdb_data = 0;
+	u16 rcv_data = 0;
+	int ret = 0;
+	struct spi_transfer t = {
+		.tx_buf = NULL,
+		.rx_buf = &rx,
+		.len = 2,
+	};
+
+	reg_rdb_data = FIELD_PREP(AD5592R_S_EN_READB, 1) |
+		       FIELD_PREP(AD5592R_S_REG_RD, addr);
+
+	ret = ad5592r_s_spi_write(st, AD5592R_S_REG_RDB_ADDR, reg_rdb_data);
+
+	if (ret) {
+		dev_info(&st->spi->dev,
+			"WRITE to READBACK register failed with error code %d\n",
+			ret);
+		return ret;
+	}
+
+	ret = spi_sync_transfer(st->spi, &t, 1);
+
+	if (ret) {
+		dev_info(&st->spi->dev,
+			"READ from READBACK failed with error code %d\n",
+			ret);
+		return ret;
+	}
+
+
+	rcv_data = get_unaligned_be16(&rx);
+	*data = FIELD_GET(AD5592R_S_REG_DATA_MSK, rcv_data);
+
+	return 0;
+}
+
+static int ad5592r_s_debugfs_reg_access(struct iio_dev *indio_dev,
+					  unsigned int reg,
+					  unsigned int writeval,
+					  unsigned int *readval)
+{
+	struct adc_ad5592r_s_st *st = iio_priv(indio_dev);
+
+	if (readval) {
+		return ad5592r_s_spi_read(st, reg, (u16 *)readval);
+	}
+
+	return ad5592r_s_spi_write(st, reg, writeval);
+}
 
 static int adc_ad5592r_s_read_raw(struct iio_dev *indio_dev,
 				  struct iio_chan_spec const *chan, int *val,
@@ -164,7 +259,8 @@ static const struct iio_chan_spec adc_ad5592r_s_channels[] = {
 
 static const struct iio_info ad5592r_s_info = {
 	.read_raw = &adc_ad5592r_s_read_raw,
-	.write_raw = &adc_ad5592r_s_write_raw
+	.write_raw = &adc_ad5592r_s_write_raw,
+	.debugfs_reg_access = &ad5592r_s_debugfs_reg_access,
 };
 
 static int ad5592r_s_probe(struct spi_device *spi)
@@ -176,7 +272,12 @@ static int ad5592r_s_probe(struct spi_device *spi)
 
 	st = iio_priv(indio_dev);
 	st->reg_select = 1;
-	memset(st->chan_val, 0, sizeof(sizeof(st->chan_val)));
+	st->spi = spi;
+	memset(st->chan_val, 0, sizeof(st->chan_val));
+
+	ad5592r_s_spi_write(st,
+			    AD5592R_S_PD_ADDR,
+			    FIELD_PREP(AD5592R_S_PD_IREF, 1));
 
 	indio_dev->name = "ad5592r_s";
 	indio_dev->info = &ad5592r_s_info;
