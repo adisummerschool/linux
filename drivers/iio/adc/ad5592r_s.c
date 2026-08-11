6495207eb9c15e7d91787d5aff76c6a7614e0be6
Pelin Mihai <mihai.pelin01@gmail.com>
2026-08-11 16:59:09 +0300

    drivers: iio: adc: add SPI communication and debugfs
    - Add SPI read/write routines.
    - Implement debugfs_reg_access for register debugging.
    - Enable internal voltage reference (IREF) on probe.
    - Add 'spi-cpol' to device tree for correct clock polarity.
    
    Signed-off-by: Pelin Mihai <mihai.pelin01@gmail.com>

--- drivers/iio/adc/ad5592r_s.c
+++ drivers/iio/adc/ad5592r_s.c
@@ -5,15 +5,104 @@
  * Copyright 2025 Analog Devices Inc.
  */
 
+ #include <linux/unaligned.h>
+ #include <linux/bitfield.h>
  #include <linux/module.h>
  #include <linux/spi/spi.h>
  #include <linux/iio/iio.h>
+
+
+#define AD5592R_S_MSB_MSK BIT(15)
+#define AD5592R_S_ADDR_MSK GENMASK(14, 11)
+#define AD5592R_S_DATA_MSK GENMASK(10, 0)
+
+#define AD5592R_S_REG_RDB_ADDR 0x7
+#define AD5592R_S_EN_READB BIT(6)
+#define AD5592R_S_REG_SELECT_RDB GENMASK(5, 2)
+
+#define AD5592R_S_REG_PD_ADDR 0xB
+#define AD5592R_S_REG_EN_IREF BIT(9)
 
  struct ad5592r_s_st{
  	int reg_select;
 	
 	int chan_val[6];
+	struct spi_device *spi;
  };
+
+ 
+
+ static int ad5592r_s_spi_write(struct ad5592r_s_st *st, u8 addr, u16 data)
+ {
+	u16 tx = 0;
+	u16 package = 0;
+
+ 	struct spi_transfer t = {
+ 		.tx_buf = &package,
+ 		.len = 2, //2 Bytes pentru ca avem 16 biti de trimis
+ 	};
+
+	tx = FIELD_PREP(AD5592R_S_MSB_MSK, 0) | FIELD_PREP(AD5592R_S_ADDR_MSK, addr)
+		 | FIELD_PREP(AD5592R_S_DATA_MSK, data); 
+
+	put_unaligned_be16(tx, &package); //convertim datele in big endian pentru a fi trimise prin spi
+
+	dev_info(&st->spi->dev, "tx we constructed: %x\n", tx);
+	dev_info(&st->spi->dev, "package we constructed: %x\n", package);
+
+ 	return spi_sync_transfer(st->spi, &t, 1); //trimitem datele prin spi, 1 pentru ca avem 1 structura de tip transfer
+
+ }
+
+static int ad5592r_s_spi_read(struct ad5592r_s_st *st, u8 addr, u16 *data)
+ {
+	u16 rx = 0;
+	u16 reg_rdb_data;
+	u16 rcv_data = 0;
+	int ret = 0;
+ 	struct spi_transfer t = {
+			.tx_buf = NULL,
+ 			.rx_buf = &rx,
+ 			.len = 2,
+ 	};
+
+	reg_rdb_data = FIELD_PREP(AD5592R_S_EN_READB, 1) | FIELD_PREP(AD5592R_S_REG_SELECT_RDB, addr);
+	ret = ad5592r_s_spi_write(st, AD5592R_S_REG_RDB_ADDR, reg_rdb_data); //trimitem comanda de citire a registrului
+
+	if(ret)
+	{
+		dev_info(&st->spi->dev, "Writing the readback register failed");
+	}
+
+	ret = spi_sync_transfer(st->spi, &t,1);
+
+	if(ret)
+	{
+		dev_info(&st->spi->dev, "Failed receiving readback");
+		return ret;
+	}
+
+	rcv_data = get_unaligned_be16(&rx);
+	*data = FIELD_GET(AD5592R_S_DATA_MSK, rcv_data);
+
+	return 0;
+ }
+
+
+ static int ad5592r_s_debugfs_reg_access(struct iio_dev *indio_dev, unsigned reg, 
+											unsigned writeval, 
+											unsigned *readval)
+ {
+	struct ad5592r_s_st *st = iio_priv(indio_dev);
+
+	if (readval) {
+		return ad5592r_s_spi_read(st, reg, (u16*) readval);
+	}
+	else {
+		return ad5592r_s_spi_write(st, reg, writeval);
+	}
+	
+ }
 
  static int ad5592r_s_read_raw(struct iio_dev *indio_dev, //folosim pentru a citi datele de la driver
  				struct iio_chan_spec const *chan,
@@ -101,15 +190,15 @@
 				}
 				return 0;
 			}
-			else
-				return -EINVAL;
-			case IIO_CHAN_INFO_ENABLE:
-				st->reg_select = val ? 1 : 0;
-				return 0;
+            else
+                return -EINVAL;
+            case IIO_CHAN_INFO_ENABLE:
+                st->reg_select = val ? 1 : 0;
+                return 0;
 
-			default:
-				return -EINVAL;
-			}
+            default:
+                return -EINVAL;
+            }
  }
 
 
@@ -162,6 +251,7 @@
  static const struct iio_info ad5592r_s_info = {
 	.read_raw = &ad5592r_s_read_raw,
 	.write_raw = &ad5592r_s_write_raw,
+	.debugfs_reg_access = &ad5592r_s_debugfs_reg_access,
  };
 
 
@@ -170,17 +260,18 @@
  	struct iio_dev *indio_dev;
 	struct ad5592r_s_st *st;
 
- 	int ret;
-
  	indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st)); //devm submodul linux, se ocupa de alocarea de memorie
+	
 	st = iio_priv(indio_dev); //returneaza pointerul catre structura noastra
 	st->reg_select = 1;
+	st->spi = spi;
 	memset(st->chan_val, 0, sizeof(st->chan_val)); //initializam valorile canalelor cu 0
     indio_dev->name = "ad5592r_s";
     indio_dev->info = &ad5592r_s_info;
 	indio_dev->channels = ad5592r_s_channels;
 	indio_dev->num_channels = ARRAY_SIZE(ad5592r_s_channels);
 
+	ad5592r_s_spi_write(st, AD5592R_S_REG_PD_ADDR, FIELD_PREP(AD5592R_S_REG_EN_IREF, 1));
  	return devm_iio_device_register(&spi->dev, indio_dev);
  }
 
