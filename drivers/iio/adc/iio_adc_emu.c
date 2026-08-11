d70529b3285046463fa365b4f152d11cb6fac7bd
Molnar Levente <mlevente388@gmail.com>
2026-08-11 13:24:36 +0300

    embeded/emu : Implemented SPI communication protocol
    -Implemented MISO MOSI for communication (used as functions)
    
    Signed-off-by: Molnar Levente <mlevente388@gmail.com>

--- drivers/iio/adc/iio_adc_emu.c
+++ drivers/iio/adc/iio_adc_emu.c
@@ -9,11 +9,92 @@
  #include <linux/spi/spi.h>
  #include <linux/iio/iio.h>
 
+ #include <linux/unaligned.h>
+ #include <linux/bitfield.h>
+
+
+
+
+ //MASKS(CHESTII NOI)
+ #define EMU_RDWR_MSK BIT(7)
+ #define EMU_ADDR_MSK GENMASK(14,8)
+ #define EMU_DATA_MSK GENMASK(7,0)
+
+
+ //CONSTANTE(CHESTII NOI)
+ #define EMU_POWER_REG 0x02
+ #define EMU_POWER_ENB 0x00
+ #define EMU_POWER_DSB 0x20
 struct iio_adc_emu_st_prv 
     {
+        struct spi_device *spi;
         int reg_select;
         int chan_val[2];
     };
+
+
+
+
+ //CHESTII NOI
+
+
+static int iio_adc_emu_spi_read(struct iio_adc_emu_st_prv *st, u8 addr, u8 *data)
+    {
+        u8 tx = 0;
+        u8 rx = 0;
+        int ret = 404;
+        struct spi_transfer t[] = {
+            {
+                .tx_buf = &tx,
+                .len = 1
+            },
+            {
+                .rx_buf = &rx,
+                .len = 1
+            }
+        };
+
+        tx = FIELD_PREP(EMU_RDWR_MSK, 1) | addr;
+        ret = spi_sync_transfer(st->spi, t, 2);
+        if(ret)
+            {
+                dev_info(&st->spi->dev, "SPI READ FAIL. ERROR CODE: %d\n", ret);
+                return ret;
+            }
+        else
+        {
+            *data = rx;
+            return ret;
+        }    
+    }
+ 
+static int iio_adc_emu_spi_write(struct iio_adc_emu_st_prv *st, u8 addr, u8 data)
+    {
+        u16 tx = 0;
+        u16 package = 0;
+        struct spi_transfer t[] = {
+            {
+                .tx_buf = &package,
+                .len = 2   
+            }
+        };
+
+        tx = FIELD_PREP(EMU_RDWR_MSK, 0) | FIELD_PREP(EMU_ADDR_MSK, addr) | FIELD_PREP(EMU_DATA_MSK,data);
+        put_unaligned_be16(tx, &package);
+
+        dev_info(&st->spi->dev,"Tx we constructed %x \n", tx);
+        dev_info(&st->spi->dev,"Package we constructed %x\n", package);
+        return spi_sync_transfer(st->spi, t, 1); 
+    }
+
+static int iio_adc_emu_debugfs_reg_access(struct iio_dev *indio_dev,unsigned reg, unsigned writeval, unsigned *readval)
+    {
+        struct iio_adc_emu_st_prv *st = iio_priv(indio_dev);
+        if(readval)
+            return iio_adc_emu_spi_read(st, reg, (u8*)readval);
+        else
+            return iio_adc_emu_spi_write(st, reg, writeval);
+    }
 
 static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,struct iio_chan_spec const *chan,int *val,int *val2,long mask)
     {
@@ -63,7 +144,16 @@ static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec 
             else
                 return -EINVAL;
             case IIO_CHAN_INFO_ENABLE:
-                st->reg_select = val ? 1:0;
+                if(val)
+                    {
+                        st->reg_select = 1;
+                        iio_adc_emu_spi_write(st, EMU_POWER_REG, EMU_POWER_DSB); 
+                    }
+                else
+                    {
+                        st->reg_select = 0;
+                        iio_adc_emu_spi_write(st, EMU_POWER_REG, EMU_POWER_ENB); 
+                    }
                 return 0; 
             default:
                 return -EINVAL;
@@ -90,6 +180,7 @@ static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec 
   static const struct iio_info iio_adc_emu_info = {
         .read_raw = &iio_adc_emu_read_raw,
         .write_raw = &iio_adc_emu_write_raw,
+        .debugfs_reg_access = & iio_adc_emu_debugfs_reg_access
   };
 
 
@@ -103,6 +194,7 @@ static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,struct iio_chan_spec 
 
         st = iio_priv(indio_dev);
         st->reg_select = 1;
+        st->spi = spi;
         memset(st->chan_val, 0 , sizeof(st->chan_val));
         indio_dev->name = "iio_adc_emu";
         indio_dev->info = &iio_adc_emu_info;
