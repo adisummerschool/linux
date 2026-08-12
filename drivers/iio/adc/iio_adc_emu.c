6bba00e4ef9bd9658a56843bcefc347136435e64
Cucu Maria-Virginia <mariavirginia.cucu@gmail.com>
2026-08-12 16:47:02 +0300

    drivers:iio:adc: Buffer for emulator
    - added and tested buffer functionality for emulator
    
    Signed-off-by: Cucu Maria-Virginia <mariavirginia.cucu@gmail.com>

--- drivers/iio/adc/iio_adc_emu.c
+++ drivers/iio/adc/iio_adc_emu.c
@@ -10,6 +10,9 @@
  #include <linux/spi/spi.h>
  #include <linux/iio/iio.h>
  #include <linux/bitfield.h>
+ #include <linux/iio/triggered_buffer.h>
+ #include <linux/iio/trigger_consumer.h>
+
 
  #define EMU_RDWR_MSK BIT(7)
  #define EMU_ADDR_MASK GENMASK(14, 8)
@@ -184,7 +187,45 @@
              return -EINVAL;
    }
   }                               
-                              
+ static irqreturn_t iio_adc_emu_trigger_handler(int irq, void *p)
+ {
+   struct iio_poll_func *pf = p;
+   struct iio_dev *indio_dev = pf->indio_dev;
+   struct iio_adc_emu_st *st = iio_priv(indio_dev);
+   int bit;
+   int ret;
+   u8 high, low;
+   int i = 0;
+   u16 buf[2];
+   
+     ret = iio_adc_emu_spi_write(st, EMU_REG_CNVST, FIELD_PREP(EMU_CNVS_START, 1));
+   if (ret) {
+      dev_err(&st->spi->dev, "Writing conversion in buffer failed: %d\n", ret);
+      iio_trigger_notify_done(indio_dev->trig);
+      return IRQ_HANDLED;
+   }
+
+   for_each_set_bit(bit, indio_dev->active_scan_mask, indio_dev->num_channels)
+   {
+     ret = iio_adc_emu_spi_read(st, EMU_REG_CHAN_HIGH(bit), &high);
+   if (ret) {
+      dev_err(&st->spi->dev, "Reading high register failed in trigger: %d\n", ret);
+      iio_trigger_notify_done(indio_dev->trig);
+      return IRQ_HANDLED;
+   }
+
+    ret = iio_adc_emu_spi_read(st, EMU_REG_CHAN_LOW(bit), &low);
+   if (ret) {
+      dev_err(&st->spi->dev, "Reading low register failed in trigger: %d\n", ret);
+      iio_trigger_notify_done(indio_dev->trig);
+      return IRQ_HANDLED;
+   }
+    buf[i++] = FIELD_PREP(EMU_HIGH_DATA_MSK, high) | low;
+   }
+   iio_push_to_buffers(indio_dev, buf);
+   iio_trigger_notify_done(indio_dev->trig);
+   return IRQ_HANDLED;
+ }                         
 
  static const struct iio_chan_spec iio_adc_emu_channels[] = {
    {
@@ -192,14 +233,26 @@
    .channel = 0,
    .indexed = 1,
    .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-   .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
+   .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+   .scan_index = 0,
+   .scan_type = {
+      .sign = 'u',
+      .realbits = 12,
+      .storagebits = 16
+   }
  },
   {
    .type = IIO_VOLTAGE,
    .channel = 1,
    .indexed = 1,
    .info_mask_separate = BIT(IIO_CHAN_INFO_RAW),
-   .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE)
+   .info_mask_shared_by_all = BIT(IIO_CHAN_INFO_ENABLE),
+   .scan_index = 1,
+   .scan_type = {
+      .sign = 'u',
+      .realbits = 12,
+      .storagebits = 16
+   }
  }
 
 };
@@ -214,6 +267,7 @@
  {
     struct iio_dev *indio_dev;
     struct iio_adc_emu_st *st;
+    int ret;
 
     indio_dev = devm_iio_device_alloc(&spi->dev, sizeof(*st));
 
@@ -225,7 +279,12 @@
     indio_dev->info = &iio_adc_emu_info;
     indio_dev->channels = iio_adc_emu_channels;
     indio_dev->num_channels = ARRAY_SIZE(iio_adc_emu_channels);
-
+    ret = devm_iio_triggered_buffer_setup(&spi->dev, indio_dev, NULL, &iio_adc_emu_trigger_handler, NULL);
+    if(ret)
+    {
+      dev_err(&spi->dev, "Failed to craete buffer");
+      return ret;
+    }
     return devm_iio_device_register(&spi->dev, indio_dev);
  }
 
