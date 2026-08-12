cad7179b5e58e5a8f34554898e6d32206ee3207e
Klampfl Helmut <helmutklampfl334@gmail.com>
2026-08-12 10:03:47 +0300

    emu/drivers/iio/adc: read conversion data from channels on read_raw
    -added a function for triggering the conversion and reading the channel data
    +func called in read_raw to read channel data
    
    Signed-off-by: Klampfl Helmut <helmutklampfl334@gmail.com>

--- drivers/iio/adc/iio_adc_emu.c
+++ drivers/iio/adc/iio_adc_emu.c
@@ -4,13 +4,21 @@
 #include <linux/unaligned.h>
 #include <linux/bitfield.h>
 
-#define EMU_RDWR_MSK	BIT(7)
-#define EMU_ADDR_MSK	GENMASK(14,8)
-#define EMU_DATA_MSK	GENMASK(7,0)
+#define EMU_RDWR_MSK		BIT(7)
+#define EMU_ADDR_MSK		GENMASK(14,8)
+#define EMU_DATA_MSK		GENMASK(7,0)
 
-#define EMU_PWR_REG	0x02
-#define EMU_PWR_ENABLE	0x0
-#define EMU_PWR_DISABLE	0x20
+#define EMU_PWR_REG		0x02
+#define EMU_PWR_ENABLE		0x0
+#define EMU_PWR_DISABLE		0x20
+
+#define EMU_REG_CNVST		0x03
+#define EMU_CNVST_START		BIT(0)
+
+#define EMU_REG_CHAN_HIGH(x)	(0x04 + (2*(x)))
+#define EMU_REG_CHAN_LOW(x)	(0x05 + (2*(x)))
+
+#define EMU_HIGH_DATA_MSK	GENMASK(11,8)
 
 struct iio_adc_emu_st {
 	struct spi_device *spi;
@@ -65,6 +73,38 @@ static int iio_adc_emu_spi_write(struct iio_adc_emu_st *st, u8 addr, u8 data)
 
 }
 
+static int iio_adc_emu_read_chan(struct iio_adc_emu_st *st, int channel,
+				 u16 *readval)
+{
+	u8 high, low;
+	u16 data = 0;
+	int ret;
+
+	ret = iio_adc_emu_spi_write(st, EMU_REG_CNVST, 
+				    FIELD_PREP(EMU_CNVST_START, 1));
+	if(ret){
+		dev_err(&st->spi->dev, "Writing conversion reg failed %d\n",
+			 ret);
+		return ret;
+	}
+
+	ret = iio_adc_emu_spi_read(st, EMU_REG_CHAN_HIGH(channel), &high);
+	if(ret){
+		dev_err(&st->spi->dev, "Reading high reg failed %d\n", ret);
+		return ret;
+	}
+
+	ret = iio_adc_emu_spi_read(st, EMU_REG_CHAN_LOW(channel), &low);
+	if(ret){
+		dev_err(&st->spi->dev, "Reading low reg failed %d\n", ret);
+		return ret;
+	}
+
+	data = FIELD_PREP(EMU_HIGH_DATA_MSK, high) | low;
+	*readval = data;
+	return 0;
+}
+
 static int iio_adc_emu_debugfs_reg_access(struct iio_dev *indio_dev,
 					unsigned reg, unsigned writeval,
 					unsigned *readval)
@@ -82,14 +122,15 @@ static int iio_adc_emu_read_raw(struct iio_dev *indio_dev,
 				int *val2, long mask)
 {
 	struct iio_adc_emu_st *st = iio_priv(indio_dev);
+	int ret;
 
 	switch (mask) {
 	case IIO_CHAN_INFO_RAW:
 		if (!st->reg_select) {
-			if (chan->channel)
-				*val = st->chan_val[1];
-			else
-				*val = st->chan_val[0];
+			ret = iio_adc_emu_read_chan(st, chan->channel, (u16 *)val);
+			if(ret){
+				dev_err(&st->spi->dev, "Reading from channels failed");
+			}
 			return IIO_VAL_INT;
 		} else
 			return -EINVAL;
@@ -113,11 +154,11 @@ static int iio_adc_emu_write_raw(struct iio_dev *indio_dev,
 			if (chan->channel) {
 				dev_info(&indio_dev->dev,
 					 "Trying to write to channel 2");
-				st->chan_val[1] = val;
+				// st->chan_val[1] = val;
 			} else {
 				dev_info(&indio_dev->dev,
 					 "Trying to write to channel 1");
-				st->chan_val[0] = val;
+				// st->chan_val[0] = val;
 			}
 			return 0;
 		} else
